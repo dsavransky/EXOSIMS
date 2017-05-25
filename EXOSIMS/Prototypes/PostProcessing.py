Fixed FA WA line in SurveySim with silent failure, adjusted probability calculations for PP module

EXOSIMS/Prototypes/PostProcessing.py
@@ -115,9 +115,9 @@ class PostProcessing(object):
             SNRmin (float):
                 signal-to-noise ratio threshold for detection
             bs_density (float):
-                background source density for the sInd in question in arcsec**2
+                background source density for the sInd in question in 1/arcsec**2
             OWA (float):
-                Outer Working Angle of the observation mode
+                Outer Working Angle of the observation mode in arcsec
         
         Returns:
             FA (boolean):
@@ -141,8 +141,9 @@ class PostProcessing(object):
         MD = np.array([False]*len(SNR))
         
         # 1/ For the whole system: is there a False Alarm (false positive)?
-        p = np.random.rand()
-        if p <= self.FAP + FABP:
+        p1 = np.random.rand()
+        p2 = np.random.rand()
+        if p1 <= self.FAP  or p2 <= FABP:
             FA = True
         
         # 2/ For each planet: is there a Missed Detection (false negative)?

EXOSIMS/Prototypes/SurveySimulation.py
@@ -428,8 +428,6 @@ class SurveySimulation(object):
                 if self.starRevisit.size != 0:
                     dt_max = 1.*u.week
                     dt_rev = np.abs(self.starRevisit[:,1]*u.day - TK.currentTimeNorm)
-                    print(dt_max)
-                    print(dt_rev)
                     ind_rev = [int(x) for x in self.starRevisit[dt_rev < dt_max,0] if x in sInds]
                     tovisit[ind_rev] = True
                 sInds = np.where(tovisit)[0]
@@ -578,7 +576,6 @@ class SurveySimulation(object):
         FA, MD = PPro.det_occur(SNRobs, mode['SNR'], self.bs_density[sInd], mode['OWA'])
         detected = observable
         SNR = np.zeros(len(pInds))
-        print(detected)
         if np.any(obs):
             detected[obs] = (~MD).astype(int)
             if FA == True:
@@ -591,11 +588,9 @@ class SurveySimulation(object):
         # If planets are detected, calculate the minimum apparent separation
         smin = None
         det = np.in1d(detected, [1,2])
-        print(detected)
 
         if np.any(det):
             smin = np.min(SU.s[pInds[det]])
-            print(smin)
             Logger.info('   - Detected planet(s) %s (%s/%s)'%(pInds[det],len(pInds[det]),len(pInds)))
             print '   - Detected planet(s) %s (%s/%s)'%(pInds[det],len(pInds[det]),len(pInds))
         
@@ -606,8 +601,8 @@ class SurveySimulation(object):
         # In case of a FA, generate a random delta mag (between maxFAfluxratio and
         # dMagLim) and working angle (between IWA and min(OWA, a_max))
         if FA == True:
-            WA = np.random.uniform(mode['IWA'].to('mas'), np.minimum(mode['OWA'], \
-                    np.arctan(max(PPop.arange)/TL.dist[sInd])).to('mas'))                           #XXXXXX BROKEN
+            WA = np.random.uniform(mode['IWA'].to('mas').value, np.minimum(mode['OWA'], \
+                    np.arctan(max(PPop.arange)/TL.dist[sInd])).to('mas').value)
             dMag = np.random.uniform(-2.5*np.log10(PPro.maxFAfluxratio(WA*u.mas)), OS.dMagLim)
             fEZ = ZL.fEZ0.to('1/arcsec2').value
             self.lastDetected[sInd,0] = np.append(self.lastDetected[sInd,0], True)
@@ -615,7 +610,6 @@ class SurveySimulation(object):
             self.lastDetected[sInd,2] = np.append(self.lastDetected[sInd,2], dMag)
             self.lastDetected[sInd,3] = np.append(self.lastDetected[sInd,3], WA)
             sminFA = np.tan(WA*u.mas)*TL.dist[sInd].to('AU')
-            print(sminFA)
             smin = np.minimum(smin,sminFA) if smin is not None else sminFA
             Logger.info('   - False Alarm (WA=%s, dMag=%s)'%(int(WA)*u.mas,round(dMag,1)))
             print '   - False Alarm (WA=%s, dMag=%s)'%(int(WA)*u.mas,round(dMag,1))
@@ -631,7 +625,6 @@ class SurveySimulation(object):
             else:
                 Mp = SU.Mp.mean()
             mu = const.G*(Mp + Ms)
-            print(sp)
             T = 2.*np.pi*np.sqrt(sp**3/mu)
             t_rev = TK.currentTimeNorm + T/2.
         # Otherwise, revisit based on average of population semi-major axis and mass
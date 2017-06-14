5e7d87e0d4f0332983da14f79f8c6b6277fe944e
Christian <christian.delacroix@gmail.com>
2017-06-14 15:33:53 -0400

    Revert last commit (unintended)

--- EXOSIMS/PlanetPopulation/EarthTwinHabZone1.py
+++ EXOSIMS/PlanetPopulation/EarthTwinHabZone1.py
@@ -12,9 +12,9 @@ class EarthTwinHabZone1(PlanetPopulation):
     of JSON inputs.
     """
 
-    def __init__(self, **specs):
+    def __init__(self, eta=0.1, **specs):
         
-        specs['eta'] = 0.1
+        specs['eta'] = eta
         specs['arange'] = [0.7, 1.5]
         specs['erange'] = [0,0]
         specs['prange'] = [0.367,0.367]

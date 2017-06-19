fd9de1d6c3f50fc2f69996e4750dea34fe6d96ed
Dmitry Savransky <dsavransky@gmail.com>
2017-06-19 15:02:49 -0400

    adding alternate stm implementation for fallback

--- EXOSIMS/util/keplerSTM.py
+++ EXOSIMS/util/keplerSTM.py
@@ -47,21 +47,29 @@ class planSys:
             self.mu = np.array(mu)
         
         self.epsmult = epsmult
+        
+        #create position and velocity index matrices
+        tmp = np.reshape(np.arange(self.nplanets*6),(self.nplanets,6)).T
+        self.rinds = tmp[0:3]
+        self.vinds = tmp[3:6]
+
+
         self.updateState(np.squeeze(x0))
 
     def updateState(self,x0):
-        self.x0 = x0
-        
-        #create position and velocity matrices
-        tmp = np.reshape(self.x0,(self.nplanets,6)).T
-        r0 = tmp[0:3]
-        v0 = tmp[3:6]
-        
-        #constants and allocation
-        self.r0norm = np.sqrt(sum(r0**2.,0))
-        self.nu0 = sum(r0*v0,0)
-        self.beta = 2*self.mu/self.r0norm - sum(v0**2, 0)
+        self.x0 = x0        
+        r0 = self.x0[self.rinds]
+        v0 = self.x0[self.vinds]
+ 
+        #constants
+        self.r0norm = np.sqrt(np.sum(r0**2.,0)) #||r0||
+        self.v0norm2 = np.sum(v0*v0,0)          #||v0||^2
+        self.nu0 = np.sum(r0*v0,0)              #r0 \cdot v0
+        self.beta = 2*self.mu/self.r0norm - self.v0norm2 #-2E
+        self.alpha = self.beta/self.mu
+        self.nu0osmu = self.nu0/np.sqrt(self.mu)
 
+        
     def takeStep(self,dt):
         Phi = self.calcSTM(dt)
         self.updateState(np.dot(Phi,self.x0))
@@ -141,3 +149,95 @@ class planSys:
             raise ValueError('Failed to converge on G, most likely due to divergence in continued fractions.')
         
         return G
+
+    def calcSTM_vallado(self,dt):
+        #classify orbits
+        epsval = 1e-12
+
+        eorbs = self.alpha >= epsval
+        porbs = np.abs(self.alpha) < epsval
+        horbs = self.alpha <= -epsval
+        
+        xi = np.zeros(self.nplanets)
+        if np.any(eorbs):
+            atmp = self.alpha[eorbs]
+            tmp = np.sqrt(self.mu[eorbs])*dt*atmp
+            circinds = np.abs(atmp - 1) > epsval
+            if any(circinds):
+                tmp[circinds] *= 0.97
+            
+            xi[eorbs] = tmp
+
+        if np.any(porbs):
+            r0 = self.x0[self.rinds]
+            v0 = self.x0[self.vinds]
+
+            h = np.cross(r0[:,porbs].T,v0[:,porbs].T).T
+            p = np.sum(h*h,0)/self.mu[porbs]
+        
+            s = np.arctan2(1.0,(3.0*np.sqrt(self.mu[porbs]/p**3.0)*dt))/2.0
+            w = np.arctan((np.tan(s))**(1./3.))
+            xi[porbs] = sqrt(p)*2./tan(2*w)
+            self.alpha[porbs] = 0
+
+        if np.any(horbs):
+            a = 1./(self.alpha[horbs])
+            xi[horbs] = np.sign(dt)*np.sqrt(-a)*np.log(-2*self.mu[horbs]*self.alpha[horbs]*dt/\
+                    (self.nu0[horbs] + np.sign(dt)*np.sqrt(-self.mu[horbs]*self.alpha[horbs])*\
+                    (1.0 - self.r0norm[horbs]*self.alpha[horbs])))
+
+
+        #loop
+        counter = 0
+        r = self.r0norm
+        xiup = 10.*np.max(np.abs(np.hstack((xi,r))))
+        while (np.max(np.abs(xiup)) > self.epsmult*np.spacing(np.max(np.abs(np.hstack((xi,r)))))) and (counter < 1000):
+            ps = xi**2.0*self.alpha
+            c2,c3 = self.psi2c2c3(ps)
+            r = xi**2.0*c2 +  self.nu0osmu*xi*(1 - ps*c3) + self.r0norm*(1 - ps*c2)
+            xiup = (np.sqrt(self.mu)*dt - xi**3.0*c3 - self.nu0osmu*xi**2.0*c2 - self.r0norm*xi*(1 - ps*c3))/r
+            xi += xiup
+            counter += 1
+
+                
+        if (counter == 1000):
+            raise ValueError('Failed to converge on xi: %e/%e'%(np.max(np.abs(xiup)), self.epsmult*np.spacing(np.max(np.abs(np.hstack((xi,r)))))))
+
+        
+        #kepler solution
+        f = 1.0 - xi**2.0/self.r0norm*c2
+        g = dt - xi**3.0/np.sqrt(self.mu)*c3
+        F = np.sqrt(self.mu)/r/self.r0norm*xi*(ps*c3 - 1.0)
+        G = 1.0 - xi**2.0/r*c2
+
+        Phi = np.zeros([6*self.nplanets]*2)
+        for j in np.arange(self.nplanets):
+            st = j*6
+            Phi[st:st+6,st:st+6] = np.vstack((np.hstack((np.eye(3)*f[j], np.eye(3)*g[j])),np.hstack((np.eye(3)*F[j], np.eye(3)*G[j]))))
+        
+        return Phi
+
+
+    def psi2c2c3(self, psi0):
+
+        c2 = np.zeros(len(psi0))
+        c3 = np.zeros(len(psi0))
+
+        psi12 = np.sqrt(np.abs(psi0))
+        pos = psi0 >= 0
+        neg = psi0 < 0
+        if np.any(pos):
+            c2[pos] = (1 - np.cos(psi12[pos]))/psi0[pos]
+            c3[pos] = (psi12[pos] - np.sin(psi12[pos]))/psi12[pos]**3.
+        if any(neg):
+            c2[neg] = (1 - inp.cosh(psi12[neg]))/psi0[neg]
+            c3[neg] = (np.sinh(psi12[neg]) - psi12[neg])/psi12[neg]**3.
+
+        tmp = c2+c3 == 0
+        if any(tmp):
+            c2[tmp] = 1./2.
+            c3[tmp] = 1./6.
+
+        return c2,c3
+
+

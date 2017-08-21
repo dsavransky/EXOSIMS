06bcfd8339ca69f347b5254751073a49835157cb
Dmitry Savransky <dsavransky@gmail.com>
2017-08-21 13:27:47 -0400

    tests testing

---
+++ tests/test0.py
@@ -0,0 +1,10 @@
+import unittest
+import EXOSIMS
+
+class TestStringMethods(unittest.TestCase):
+    
+    def test0(self):
+        self.assertEqual(0,0)
+
+if __name__ == '__main__':
+    unittest.main()

4f8e7fa159a71bfb829c00673b77b730cd258762
Dmitry Savransky <dsavransky@gmail.com>
2017-08-22 13:54:06 -0400

    adding util tests and fixing genOutSpec bug in case where EXOSIMS is imported from local dir

---
+++ tests/TestModules/PlanetPopulation/PlanetPopulationErrorNaming.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from EXOSIMS.Prototypes.PlanetPopulation import PlanetPopulation
+
+# Note: wrong class name has been given within the module (they should match)
+class PlanetPopulationErrorNamingBadName(PlanetPopulation):
+    """PlanetPopulation non-prototype class template (error-containing).
+    
+    This class exists to test import of non-EXOSIMS modules using
+    the get_module interface.  It presently takes no arguments
+    and does nothing besides initialize itself.
+
+    The class has an erroneous name: the class implemented here has a different
+    name from the module.
+    
+    Args:
+        \*\*specs:
+            user specified values
+            
+    Attributes:
+        (none)
+    """
+
+    _modtype = 'PlanetPopulation'
+    _outspec = {}
+
+    def __init__(self, **specs):
+        pass
+
---
+++ tests/TestModules/PlanetPopulation/PlanetPopulationModified.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from EXOSIMS.Prototypes.PlanetPopulation import PlanetPopulation
+
+class PlanetPopulationModified(PlanetPopulation):
+    """PlanetPopulation non-prototype class template.
+    
+    This class exists to test import of non-EXOSIMS modules using
+    the get_module interface.  It presently takes no arguments
+    and does nothing besides initialize itself.
+    
+    Args:
+        \*\*specs:
+            user specified values
+            
+    Attributes:
+        (none)
+    """
+
+    _modtype = 'PlanetPopulation'
+    _outspec = {}
+
+    def __init__(self, **specs):
+        pass
+

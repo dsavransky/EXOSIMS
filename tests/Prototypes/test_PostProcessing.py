971f9c2e8899960686b8107b7fc8f82d61724540
dsavransky <dsavransky@gmail.com>
2017-08-29 17:01:54 -0400

    updating postprocessing tests to account for det_occur interface change

--- tests/Prototypes/test_PostProcessing.py
+++ tests/Prototypes/test_PostProcessing.py
@@ -15,6 +15,10 @@ specs = {'modules':{'BackgroundSources':''}}
 
 
 class Test_PostProcessing_prototype(unittest.TestCase):
+
+    def setUp(self):
+        self.TL = {}
+        self.mode = {'SNR':5.0}
     
     #Testing some limiting cases below
     def test_zeroFAP(self):
@@ -22,9 +26,8 @@ class Test_PostProcessing_prototype(unittest.TestCase):
         obj = PostProcessing.PostProcessing(FAP=0.0,MDP=0.0,**specs)
 
         SNRin = np.array([1.0,2.0,3.0,4.0,5.0,5.1,6.0])
-        SNRmin = 5.0
         expected_MDresult = [True,True,True,True,False,False,False] 
-        FA, MD = obj.det_occur(SNRin,SNRmin)     
+        FA, MD = obj.det_occur(SNRin,self.mode,self.TL,0,0)     
         for x,y in zip(MD,expected_MDresult):
             self.assertEqual( x,y )
         self.assertEqual( FA, False)
@@ -35,9 +38,8 @@ class Test_PostProcessing_prototype(unittest.TestCase):
         obj = PostProcessing.PostProcessing(FAP=1.0,MDP=0.0,**specs)
 
         SNRin = np.array([1.0,2.0,3.0,4.0,5.0,5.1,6.0])
-        SNRmin = 5.0
         expected_MDresult = [True,True,True,True,False,False,False] 
-        FA, MD = obj.det_occur(SNRin,SNRmin)        
+        FA, MD = obj.det_occur(SNRin,self.mode,self.TL,0,0)        
         for x,y in zip(MD,expected_MDresult):
             self.assertEqual( x,y )
         self.assertEqual( FA, True)
@@ -49,7 +51,7 @@ class Test_PostProcessing_prototype(unittest.TestCase):
         #Test a case for which the false alarm prob is 0.1
         FAs = np.zeros(1000,dtype=bool)
         for j in range(len(FAs)):
-            FA,_ = obj.det_occur(np.array([5]),5)
+            FA,_ = obj.det_occur(np.array([5]),self.mode,self.TL,0,0)
             FAs[j] = FA
         #~0.3% of the time this test should fail! due to random number gen.
         np.testing.assert_allclose(len(FAs)*obj.FAP, len(np.where(FAs)[0]), rtol=0.3, atol=0.)

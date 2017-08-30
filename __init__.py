b7da2403ce1d6b4fd21e5b03c84e353b2a4f91e3
Dean Keithly <drk94@cornell.edu>
2017-08-30 11:15:21 -0400

    Reverting commit 1171669

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# Set up a default logging handler to avoid "No handler found" warnings.
-# Other handlers can add to this one.
-import logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())

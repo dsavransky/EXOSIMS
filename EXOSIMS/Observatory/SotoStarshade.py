removing debugging code and fixing star angular separation in calculate_slewTimes() and fixing mass formula in log_occulterResults()

EXOSIMS/Observatory/SotoStarshade.py
@@ -16,7 +16,7 @@ class SotoStarshade(ObservatoryL2Halo):
     """
     
     def __init__(self, missionStart=60634., maxdVpcnt=0.02,
-            setTOF=20,orbit_datapath=None,**specs): 
+            setTOF=14,orbit_datapath=None,**specs): 
 
         ObservatoryL2Halo.__init__(self,**specs)
 
@@ -297,20 +297,21 @@ class SotoStarshade(ObservatoryL2Halo):
     
         dV = np.zeros(TL.nStars)*u.m/u.s  
         sd = np.zeros(TL.nStars)*u.deg
-        
-        finalTime = currentTime + self.setTOF[0]*u.d
             
         if old_sInd is None:
             old_sInd = np.random.randint(0,TL.nStars)
-
-        for x in sInd:
-            sd[x] = self.star_angularSep(TL,old_sInd,x,currentTime,finalTime)*u.deg
-            dV[x] = self.calculate_dV(self.setTOF,TL,old_sInd,x,currentTime)*u.m/u.s
             
-            if (x % 100) == 0:
-                print '  Calculation ',x,' yields '
-                print '   ',sd[x]
-                print '   ',dV[x]
+        # position vector of previous target star
+        r_old = TL.starprop(old_sInd, currentTime)[0]
+        u_old = r_old.value/np.linalg.norm(r_old)
+        # position vector of new target stars
+        r_new = TL.starprop(sInd, currentTime)
+        u_new = (r_new.value.T/np.linalg.norm(r_new, axis=1)).T
+        # angle between old and new stars
+        sd = np.arccos(np.clip(np.dot(u_old, u_new.T), -1, 1))*u.rad
+        
+        for x in sInd:
+            dV[x] = self.calculate_dV(self.setTOF,TL,old_sInd,x,currentTime)*u.m/u.s
             
         slewTimes = self.setTOF[0]*np.ones(TL.nStars)*u.d
         sInd = sInd[np.where(dV.value < self.dVmax.value)]
@@ -344,7 +345,7 @@ class SotoStarshade(ObservatoryL2Halo):
         DRM['slew_angle'] = sd[sInd].to('deg')
         
         dV = dV[sInd].to('m/s')
-        slew_mass_used = self.scMass * ( np.exp(-dV.value/(self.slewIsp.value*const.g0.value)) - 1)
+        slew_mass_used = self.scMass * ( 1 - np.exp(-dV.value/(self.slewIsp.value*const.g0.value)))
         DRM['slew_dV'] = dV
         DRM['slew_mass_used'] = slew_mass_used.to('kg')
         self.scMass = self.scMass - slew_mass_used
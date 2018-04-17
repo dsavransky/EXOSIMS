1d647dc04e9962bad59bc0a0aedd66056ff27de0
Dean Keithly <drk94@cornell.edu>
2018-04-17 14:18:52 -0400

    removing starkAYO from module exception list

--- tests/SimulatedUniverse/test_SimulatedUniverse.py
+++ tests/SimulatedUniverse/test_SimulatedUniverse.py
@@ -38,7 +38,7 @@ class TestSimulatedUniverse(unittest.TestCase):
         pkg = EXOSIMS.SimulatedUniverse
         self.allmods = [get_module(modtype)]
         for loader, module_name, is_pkg in pkgutil.walk_packages(pkg.__path__, pkg.__name__+'.'):
-            if (not 'starkAYO' in module_name) and not is_pkg:
+            if not is_pkg:
                 mod = get_module(module_name.split('.')[-1],modtype)
                 self.assertTrue(mod._modtype is modtype,'_modtype mismatch for %s'%mod.__name__)
                 self.allmods.append(mod)

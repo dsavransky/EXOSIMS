d7644e639dc6d7a9e4e9d8d7276677a13d739663
Dean Keithly <drk94@cornell.edu>
2018-05-08 16:25:02 -0400

    still in progress

--- EXOSIMS/util/LoadMissionFromSeed.py
+++ EXOSIMS/util/LoadMissionFromSeed.py
@@ -56,46 +56,82 @@ import json
 #Load DRM from pkl file and compare to sim run with same seed
 pklfile = "/home/dean/Documents/SIOSlab/Dean2May18RS16CXXfZ01OB01PP01SU01/run979296152263.pkl"
 outspecfile = "/home/dean/Documents/SIOSlab/Dean2May18RS16CXXfZ01OB01PP01SU01/outspec.json"
-with open(pklfile, 'rb') as f:#load from cache
+with open(pklfile, 'rb') as f:#load pkl to extract mission specific SEED
     DRM3tmp = pickle.load(f)
 
 DRM3seed = DRM3tmp['seed']
 DRM3 = DRM3tmp['DRM']
 DRM3systems = DRM3tmp['systems']
+#Spot Logic Check to ensure DRM3systems plan_inds have the correct star##############
+#planet indices for DRM3
+pInds = DRM3[0]['plan_inds']
+#All plan_inds should have the same star. Confirm with this
+DRM3systems['star'][pInds] #all names should be identical
 
 
+#Load the Outspec File for Modification#######################
 with open(outspecfile, 'rb') as f:#load from cache
     newjson = json.load(f)
-newjson['seed'] = DRM3seed
-newjson['modules']['SurveyEnsemble'] = "SurveyEnsemble"
-
+newjson['seed'] = DRM3seed#Modify the outspec file
+newjson['modules']['SurveyEnsemble'] = "SurveyEnsemble"#modify the outspec file
+#Save newjson script
 with open(os.path.dirname(outspecfile)+'tmp.json', 'wb') as f:
     json.dump(newjson, f)
 
+#Initialize sim4 from modified json script
 sim4 = EXOSIMS.MissionSim.MissionSim(os.path.dirname(outspecfile) + 'tmp.json')
-SU = sim4.SurveySimulation.SimulatedUniverse
+SU1 = sim4.SurveySimulation.SimulatedUniverse
 TK = sim4.SurveySimulation.TimeKeeping
 Obs = sim4.SurveySimulation.Observatory
 SS = sim4.SurveySimulation
-TK.__init__(**newjson)
-Obs.__init__(**newjson)
-SS.__init__(**newjson)
-SU.I = DRM3systems['I']
-SU.M0 = DRM3systems['M0']
-SU.Mp = DRM3systems['Mp']
-SU.O = DRM3systems['O']
-SU.Rp = DRM3systems['Rp']
-SU.a = DRM3systems['a']
-SU.e = DRM3systems['e']
-SU.mu = DRM3systems['mu']
-SU.p = DRM3systems['p']
-SU.plan2star = DRM3systems['plan2star']
-SU.star = DRM3systems['star']
-SU.w = DRM3systems['w']
+
+with open(os.path.dirname(outspecfile) + 'tmp.json', 'rb') as f:#load from cache
+    newjson2 = json.load(f)
+
+TK.__init__(**newjson2.copy())
+Obs.__init__(**newjson2.copy())
+SS.__init__(**newjson2.copy())#The scienceInstruments become populated with units....
+print(newjson2)#For some reason, newjson2 has been populated with units... gross
+with open(os.path.dirname(outspecfile) + 'tmp.json', 'rb') as f:#load from cache
+    newjson2 = json.load(f)
+SU1.__init__(**newjson2.copy())
+
+
+print saltyburrito
+
+SU1.I = DRM3systems['I']
+SU1.M0 = DRM3systems['M0']
+SU1.Mp = DRM3systems['Mp']
+SU1.O = DRM3systems['O']
+SU1.Rp = DRM3systems['Rp']
+SU1.a = DRM3systems['a']
+SU1.e = DRM3systems['e']
+SU1.mu = DRM3systems['mu']
+SU1.p = DRM3systems['p']
+SU1.plan2star = DRM3systems['plan2star']
+SU1.star = DRM3systems['star']
+SU1.w = DRM3systems['w']
+
+#absT1 = TK.currentTimeAbs.copy()
+#SCpos1 = Obs.orbit(TK.currentTimeAbs.copy())
+#fZ1 = sim4.ZodiacalLight.fZ(Obs, sim4.TargetList, range(sim4.TargetList.nStars), absT1, sim4.SurveySimulation.mode)
+
 
 sim4.SurveySimulation.run_sim()
 SS = sim4.SurveySimulation
 DRM4 = SS.DRM
+
+#FACT This statement must be if both DRM are equivalent
+if all(DRM4[0]['plan_inds'] == pInds):
+    print('okay1')
+else:
+    print('not okay1')
+
+
+ind = DRM4[0]['star_ind']
+DRM4[0]['det_fZ']
+fZ1[ind]
+
 
 
 

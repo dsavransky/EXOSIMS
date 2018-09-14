f4bd639e44dbc6c2a86c99c98addac4e6f8d0e82
Dean Keithly <drk94@cornell.edu>
2018-09-14 15:54:53 -0400

    runQueue Error Checking, More Inputs

--- EXOSIMS/run/runQueue.py
+++ EXOSIMS/run/runQueue.py
@@ -2,6 +2,9 @@
 Top-level run script for IPCluster parallel Queue implementation
 Run as:
     python runQueue.py --outpath '/dirToCreateAllOutputFolders/'
+        --ScriptsPath '/Full path to directory containing Scripts/*.json')
+        --runLogPath '/Full path to directory to/runLog.csv')
+        --qFPath '/Full path/queuefile.json'
 
 This script is designed to load the queue.json file located in the "run" folder.
 queue.json contains the names of .json files located in ../Scripts/. to be run by this script
@@ -10,8 +13,6 @@ This script should be executed from the "run" folder
 Written by Dean Keithly 4/27/2018
 """
 import json
-from subprocess import Popen
-import subprocess
 import os
 import numpy as np
 import EXOSIMS
@@ -63,19 +64,34 @@ def run_one(genNewPlanets=True, rewindPlanets=True, outpath='.'):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run an ipcluster parallel ensemble job queue.")
     parser.add_argument('--outpath',nargs=1,type=str, help='Full path to output directory where each job directory will be saved (string).')
+    parser.add_argument('--ScriptsPath',nargs=1,type=str, help='Full path to directory containing run .json scripts (string).')
+    parser.add_argument('--runLogPath',nargs=1,type=str, help='Full path to directory to log simulations run (string).')
+    parser.add_argument('--qFPath',nargs=1,type=str, help='Full path to the queue file (string).')
 
     args = parser.parse_args()
+    if args.ScriptsPath is None:
+        ScriptsPath = '../../../Scripts/'#Default
+    else:
+        ScriptsPath = args.ScriptsPath[0]
+    if args.runLogPath is None:
+        runLogPath = '../../../cache/'#Default
+    else:
+        runLogPath = args.runLogPath[0]
+    if args.qFPath is None:
+        qPath = '../../../cache/queue.json'#Default
+    else:
+        qPath = args.qPath[0]
+    assert(os.path.isfile(ScriptsPath), 'Scripts Path: %s does not exist' %ScriptsPath)
+    assert(os.path.isfile(runLogPath), 'runLog Path: %s does not exist' %runLogPath)
+    assert(os.path.isfile(qPath), 'Queue Path: %s does not exist' %qPath)
 
-    queueName = 'queue.json'
-    with open('queue.json') as queueFile:
+    #Load Queue File
+    with open(qPath) as queueFile:
         queueData = json.load(queueFile)
 
-    error = False
-    #TODO ERROR CHECKING
-    
     #Check if Any of the Scripts have already been run...
     try:#check through log file if it exists
-        with open("runLog.csv","w+") as myfile:
+        with open(runLogPath + "runLog.csv","w+") as myfile:
             scriptsRun = map(lambda s: s.strip(), myfile.readlines())
             for scriptRun in scriptsRun:
                 if scriptRun in queueData['scriptNames']:
@@ -85,28 +101,23 @@ if __name__ == "__main__":
     except:
         pass
 
+    while(len(queueData['scriptNames']) > 0):#Iterate until there are no more 
+        outpath = args.outpath[0] + str(queueData['scriptNames'][0].split('.')[0])
+        if not os.path.isdir(outpath):#IF the directory doesn't exist
+            os.makedirs(outpath)#make directory
 
-    if error == False:
-        while(len(queueData['scriptNames']) > 0):#Iterate until there are no more 
-            outpath = args.outpath[0] + str(queueData['scriptNames'][0].split('.')[0])
-            if not os.path.isdir(outpath):#IF the directory doesn't exist
-                os.makedirs(outpath)#make directory
+        scriptfile = queueData['scriptNames'][0]
+        numRuns = queueData['numRuns'][0]
+        sim = EXOSIMS.MissionSim.MissionSim(ScriptsPath + scriptfile)
+        res = sim.genOutSpec(tofile = os.path.join(outpath,'outspec.json'))
+        kwargs = {'outpath':outpath}
+        numRuns = queueData['numRuns'][0]
+        res = sim.run_ensemble(numRuns, run_one=run_one, kwargs=kwargs)
 
-            scriptfile = queueData['scriptNames'][0]
-            numRuns = queueData['numRuns'][0]
-            sim = EXOSIMS.MissionSim.MissionSim('../Scripts/' + scriptfile)
-            res = sim.genOutSpec(tofile = os.path.join(outpath,'outspec.json'))
-            kwargs = {'outpath':outpath}
-            numRuns = queueData['numRuns'][0]
-            res = sim.run_ensemble(numRuns, run_one=run_one, kwargs=kwargs)
+        #Append ScriptName to logFile.csv
+        with open(runLogPath + "runLog.csv", "a") as myfile:
+            myfile.write(queueData['scriptNames'][0] + '\n')
 
-            #Append ScriptName to logFile.csv
-            with open("runLog.csv", "a") as myfile:
-                myfile.write(queueData['scriptNames'][0] + '\n')
-
-            queueData['scriptNames'].remove(queueData['scriptNames'][0])#remove scriptfile from list
-            queueData['numRuns'].remove(queueData['numRuns'][0])#remove numRuns from list
-        print('Done running all jobs')
-    else:
-        print('There was an error and the runs did not execute')
-        pass
+        queueData['scriptNames'].remove(queueData['scriptNames'][0])#remove scriptfile from list
+        queueData['numRuns'].remove(queueData['numRuns'][0])#remove numRuns from list
+    print('Done running all jobs')

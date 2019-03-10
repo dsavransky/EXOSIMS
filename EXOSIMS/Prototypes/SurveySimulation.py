b8030030928edfd5f238d4d009f5d6289f75ce23
Dean Keithly <drk94@cornell.edu>
2019-03-10 14:26:50 -0400

    adding keepStarCatalog logic for generating outspc

--- EXOSIMS/Prototypes/SurveySimulation.py
+++ EXOSIMS/Prototypes/SurveySimulation.py
@@ -1732,6 +1732,15 @@ class SurveySimulation(object):
                         inspect.getfile(module.__class__))
             out['modules'][mod_name] = mod_name_short
         # add catalog name
+        if keepStarCatalog == False: # We do not want to keep the star catalog object
+            if isinstance(specs['modules']['StarCatalog'],str):
+                self.StarCatalog = str(specs['modules']['StarCatalog'])
+            else:
+                #Convert from StarCatalog object to string
+                self.StarCatalog = str(self.StarCatalog.__class__.__module__).split('.')[-1]
+        else: #keepStarCatalog == True
+            pass
+
         out['modules']['StarCatalog'] = str(self.StarCatalog)
         
         # add in the SVN/Git revision

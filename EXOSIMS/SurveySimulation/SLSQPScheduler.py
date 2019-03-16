fixing reset bug caused by re-initing SurveySim before TimeKeeping (leading to bad re-calculation of absTimefZmin)

EXOSIMS/SurveySimulation/SLSQPScheduler.py
@@ -364,7 +364,7 @@ class SLSQPScheduler(SurveySimulation):
         """
         #Do Checking to Ensure There are Targetswith Positive Nonzero Integration Time
         tmpsInds = sInds
-        sInds = sInds[np.where(intTimes.value > 1e-10)]#filter out any intTimes that are essentially 0
+        sInds = sInds[np.where(intTimes.value > 1e-10)[0]]#filter out any intTimes that are essentially 0
         intTimes = intTimes[intTimes.value > 1e-10]
 
         if len(sInds) == 0:#If there are no stars... arbitrarily assign 1 day for observation length otherwise this time would be wasted
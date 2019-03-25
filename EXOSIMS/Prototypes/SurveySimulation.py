56fbf7df5149700094ed805378ddeaaa26a3492a
Dean Keithly <drk94@cornell.edu>
2019-03-25 11:47:07 -0400

    adding fZQuad calculation which adds new functionality, undocumented, does not work with prototype yet

--- EXOSIMS/Prototypes/SurveySimulation.py
+++ EXOSIMS/Prototypes/SurveySimulation.py
@@ -303,7 +303,8 @@ class SurveySimulation(object):
 
         # Precalculating intTimeFilter
         sInds = np.arange(TL.nStars) #Initialize some sInds array
-        self.valfZmin, self.absTimefZmin = self.ZodiacalLight.calcfZmin(sInds, self.Observatory, TL, self.TimeKeeping, self.mode, self.cachefname) # find fZmin to use in intTimeFilter
+        self.fZQuads = self.ZodiacalLight.calcfZmin(sInds, self.Observatory, TL, self.TimeKeeping, self.mode, self.cachefname) # find fZmin to use in intTimeFilter
+        self.valfZmin, self.absTimefZmin = self.ZodiacalLight.extractfZmin_fZQuads(self.fZQuads)
         fEZ = self.ZodiacalLight.fEZ0 # grabbing fEZ0
         dMag = self.dMagint[sInds] # grabbing dMag
         WA = self.WAint[sInds] # grabbing WA

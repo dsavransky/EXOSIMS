e0f52202a94f323d68d0cd151584a492413d34e7
Dean Keithly <drk94@cornell.edu>
2019-05-10 14:09:35 -0400

    fixing minor type in plotC0vsT0 utility

--- EXOSIMS/util/plotC0vsT0andCvsT.py
+++ EXOSIMS/util/plotC0vsT0andCvsT.py
@@ -211,7 +211,7 @@ class plotC0vsT0andCvsT(object):
             DRM['DRM'] = sim.SurveySimulation.DRM
         elif 'starkAYO' in outspec['modules']['SurveySimulation']:
             #TODO
-            initt0 = np.zeros(len(sim.SruveySimulation.schedule))
+            initt0 = np.zeros(len(sim.SurveySimulation.schedule))
             initt0[sim.SurveySimulation.schedule] = sim.SurveySimulation.t_dets
 
 

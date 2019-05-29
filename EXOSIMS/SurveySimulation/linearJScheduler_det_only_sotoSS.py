2b4408e641450bc19a13b44bc60de882fc2c2f6e
Gab <gjsoto3@gmail.com>
2019-05-29 17:40:00 -0400

    applying changes to linearJScheduler_det_only_sotoSS

--- EXOSIMS/SurveySimulation/linearJScheduler_det_only_sotoSS.py
+++ EXOSIMS/SurveySimulation/linearJScheduler_det_only_sotoSS.py
@@ -151,7 +151,7 @@ class linearJScheduler_det_only_sotoSS(SurveySimulation):
                     self.vprint('waitTime is not None')
                 else:
                     startTimes = TK.currentTimeAbs.copy() + np.zeros(TL.nStars)*u.d # Start Times of Observations
-                    observableTimes = Obs.calculate_observableTimes(TL,np.arange(TL.nStars),startTimes,self.koMap,self.koTimes,self.mode)[0]
+                    observableTimes = Obs.calculate_observableTimes(TL,np.arange(TL.nStars),startTimes,self.koMaps,self.koTimes,self.mode)[0]
                     #CASE 2 If There are no observable targets for the rest of the mission
                     if((observableTimes[(TK.missionFinishAbs.copy().value*u.d > observableTimes.value*u.d)*(observableTimes.value*u.d >= TK.currentTimeAbs.copy().value*u.d)].shape[0]) == 0):#Are there any stars coming out of keepout before end of mission
                         self.vprint('No Observable Targets for Remainder of mission at currentTimeNorm= ' + str(TK.currentTimeNorm.copy()))

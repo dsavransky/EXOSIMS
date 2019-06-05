7171b52393e4a6b9623db87ed4e8f857cb6ba6fe
Dean Keithly <drk94@cornell.edu>
2019-06-04 18:00:32 -0600

    adding fix for issue203

--- EXOSIMS/Prototypes/SurveySimulation.py
+++ EXOSIMS/Prototypes/SurveySimulation.py
@@ -579,9 +579,12 @@ class SurveySimulation(object):
 
         # 2.5 Filter stars not observable at startTimes
         try:
-            koTimeInd = np.where(np.round(startTimes[0].value)-self.koTimes.value==0)[0][0]  # find indice where koTime is startTime[0]
-            #wherever koMap is 1, the target is observable
-            sInds = sInds[np.where(np.transpose(self.koMap)[koTimeInd].astype(bool)[sInds])[0]]# filters inds by koMap #verified against v1.35
+            tmpIndsbool = list()
+            for i in np.arange(len(sInds)):
+                koTimeInd = np.where(np.round(startTimes[sInds[i]].value)-self.koTimes.value==0)[0][0] # find indice where koTime is startTime[0]
+                tmpIndsbool.append(self.koMap[sInds[i]][koTimeInd].astype(bool)) #Is star observable at time ind
+            sInds = sInds[tmpIndsbool]
+            del tmpIndsbool
         except:#If there are no target stars to observe 
             sInds = np.asarray([],dtype=int)
         

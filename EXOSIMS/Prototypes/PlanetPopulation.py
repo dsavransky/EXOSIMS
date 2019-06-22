050802c835ba2b10022c829c2fe81a1ecdc89c9e
Dmitry Savransky <dsavransky@gmail.com>
2019-06-22 13:52:48 -0700

    fixing cachedir bug by ensuring that cachedir input is always part of specs dict before it is passed to init of next module

--- EXOSIMS/Prototypes/PlanetPopulation.py
+++ EXOSIMS/Prototypes/PlanetPopulation.py
@@ -66,6 +66,9 @@ class PlanetPopulation(object):
 
         # get the cache directory
         self.cachedir = get_cache_dir(cachedir)
+        self._outspec['cachedir'] = self.cachedir
+        specs['cachedir'] = self.cachedir 
+
 
         # load the vprint function (same line in all prototype module constructors)
         self.vprint = vprint(specs.get('verbose', True))

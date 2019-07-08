51be8f3ae93e4acaf96310d51ea42211141e0274
RHONDA M MORGAN <rhonda@aftac1.jpl.nasa.gov>
2019-07-07 20:34:16 -0700

    bug fix to ETHZSDET

--- EXOSIMS/PlanetPopulation/EarthTwinHabZoneSDET.py
+++ EXOSIMS/PlanetPopulation/EarthTwinHabZoneSDET.py
@@ -2,7 +2,7 @@ from EXOSIMS.Prototypes.PlanetPopulation import PlanetPopulation
 import numpy as np
 import astropy.units as u
 
-class EarthTwinHabZone1SDET(PlanetPopulation):
+class EarthTwinHabZoneSDET(PlanetPopulation):
     """Population of Earth twins (1 R_Earth, 1 M_Eearth, 1 p_Earth)
     On circular Habitable zone orbits (0.7 to 1.5 AU)
     

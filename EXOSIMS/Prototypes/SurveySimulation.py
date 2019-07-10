b29ac477ee817ddb7cef6ad5789c8630277d3764
Gab <gjsoto3@gmail.com>
2019-07-10 17:34:44 -0400

    run_sim can now handle multiple characterizations within the same run, saves results as arrays in DRM

--- EXOSIMS/Prototypes/SurveySimulation.py
+++ EXOSIMS/Prototypes/SurveySimulation.py
@@ -447,6 +447,23 @@ class SurveySimulation(object):
                 if char_mode['SNR'] not in [0, np.inf]:
                     characterized, char_fZ, char_systemParams, char_SNR, char_intTime = \
                             self.observation_characterization(sInd, char_mode)
+                    # follow-up characterization only executed for multiple distance occulter
+                    if hasattr(self,'occulterSeps') and char_intTime is not None:
+                        # number of distances at which sInd has been observed
+                        nOccObs = np.sum(self.observedAtSep[sInd])
+                        # run loop for remaining distances
+                        while nOccObs < OS.nOcculterSeps:
+                            tmpChar, tmpfZ, tmpSysParam, tmpSNR, tmp_charIntTime = \
+                                self.observation_characterization(sInd, char_mode)
+                            # break out of loop condition - characterization failed
+                            if tmp_charIntTime is None:
+                                break
+                            # combining characterization results
+                            char_intTime  = np.vstack([char_intTime.value, tmp_charIntTime.value])*char_intTime.unit
+                            characterized = np.vstack([characterized, tmpChar])
+                            char_fZ = np.vstack([char_fZ.value, tmpfZ.value])*char_fZ.unit
+                            char_SNR = np.vstack([char_SNR, tmpSNR])
+                            char_systemParams = np.vstack([char_systemParams, tmpSysParam])
                 else:
                     char_intTime = None
                     lenChar = len(pInds) + 1 if FA else len(pInds)
@@ -454,7 +471,9 @@ class SurveySimulation(object):
                     char_SNR = np.zeros(lenChar, dtype=float)
                     char_fZ = 0./u.arcsec**2
                     char_systemParams = SU.dump_system_params(sInd)
-                assert char_intTime != 0, "Integration time can't be 0."
+                
+                intTimeStatement = all(char_intTime != 0) if len(np.array([char_intTime]).shape) > 1 else char_intTime != 0
+                assert intTimeStatement, "Integration time can't be 0."
                 # update the occulter wet mass
                 if OS.haveOcculter == True and char_intTime is not None:
                     DRM = self.update_occulter_mass(DRM, sInd, char_intTime, 'char')

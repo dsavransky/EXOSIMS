fixing units in SU proto

EXOSIMS/Prototypes/SimulatedUniverse.py
@@ -345,8 +345,8 @@ class SimulatedUniverse(object):
         # phase function, exozodi surface brightness, delta magnitude and working angle
         self.r[pInds] = x1[rind]*u.AU
         self.v[pInds] = x1[vind]*u.AU/u.day
-        self.d[pInds] = np.linalg.norm(self.r[pInds], axis=1)*self.r.unit
-        self.s[pInds] = np.linalg.norm(self.r[pInds,0:2], axis=1)*self.r.unit
+        self.d[pInds] = np.linalg.norm(self.r[pInds], axis=1)
+        self.s[pInds] = np.linalg.norm(self.r[pInds,0:2], axis=1)
         self.phi[pInds] = PPMod.calc_Phi(np.arccos(self.r[pInds,2]/self.d[pInds]))
         # self.fEZ[pInds] = ZL.fEZ(TL.MV[sInd], self.I[pInds], self.d[pInds])
         self.dMag[pInds] = deltaMag(self.p[pInds], self.Rp[pInds], self.d[pInds],
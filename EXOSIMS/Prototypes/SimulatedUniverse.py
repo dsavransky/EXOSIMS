7a1089009862620cf8e6bd5e058810cc9b75552d
Dean Keithly <drk94@cornell.edu>
2020-01-23 15:53:05 -0500

    adding Python2 compatability

--- EXOSIMS/Prototypes/SimulatedUniverse.py
+++ EXOSIMS/Prototypes/SimulatedUniverse.py
@@ -7,6 +7,7 @@ from EXOSIMS.util.get_dirs import get_cache_dir
 import numpy as np
 import astropy.units as u
 import astropy.constants as const
+import sys
 
 class SimulatedUniverse(object):
     """Simulated Universe class template
@@ -274,7 +275,10 @@ class SimulatedUniverse(object):
         self.v = (v1*(-A*r2 + B*v2)).T.to('AU/day')                 # velocity
         self.d = np.linalg.norm(self.r, axis=1)                     # planet-star distance
         self.s = np.linalg.norm(self.r[:,0:2], axis=1)              # apparent separation
-        self.phi = PPMod.calc_Phi(np.arccos(self.r[:,2]/self.d))    # planet phase
+        if sys.version_info[0] > 2:
+            self.phi = PPMod.calc_Phi(np.arccos(self.r[:,2]/self.d))    # planet phase
+        else:
+            self.phi = PPMod.calc_Phi(np.arccos(self.r[:,2].to('AU').value/self.d.to('AU').value))    # planet phase
         self.fEZ = ZL.fEZ(TL.MV[self.plan2star], self.I, self.d)    # exozodi brightness
         self.dMag = deltaMag(self.p, self.Rp, self.d, self.phi)     # delta magnitude
         self.WA = np.arctan(self.s/TL.dist[self.plan2star]).to('arcsec')# working angle

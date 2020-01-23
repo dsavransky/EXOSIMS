26c44184f2a73463c78bac5f7fc31d810966a5c1
Dean Keithly <drk94@cornell.edu>
2020-01-22 23:22:21 -0500

    removing self.r.unit

--- EXOSIMS/Prototypes/SimulatedUniverse.py
+++ EXOSIMS/Prototypes/SimulatedUniverse.py
@@ -412,8 +412,8 @@ class SimulatedUniverse(object):
         
         self.r = (A*r*np.cos(nu) + B*r*np.sin(nu)).T*u.AU           # position
         self.v = (A*v1 + B*v2).T.to('AU/day')                       # velocity
-        self.d = np.linalg.norm(self.r, axis=1)*self.r.unit         # planet-star distance
-        self.s = np.linalg.norm(self.r[:,0:2], axis=1)*self.r.unit  # apparent separation
+        self.d = np.linalg.norm(self.r, axis=1)                     # planet-star distance
+        self.s = np.linalg.norm(self.r[:,0:2], axis=1)              # apparent separation
         self.phi = PPMod.calc_Phi(np.arccos(self.r[:,2]/self.d))    # planet phase
         self.fEZ = ZL.fEZ(TL.MV[self.plan2star], self.I, self.d)    # exozodi brightness
         self.dMag = deltaMag(self.p, self.Rp, self.d, self.phi)     # delta magnitude

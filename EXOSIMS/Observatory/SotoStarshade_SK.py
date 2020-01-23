def1c99082c6daa3488c4739841de109efaa413e
Gab <gjsoto3@gmail.com>
2020-01-23 13:25:05 -0500

    new class specifically for starshade stationkeeping, inherits basic SotoStarshade for now

---
+++ EXOSIMS/Observatory/SotoStarshade_SK.py
@@ -0,0 +1,32 @@
+from EXOSIMS.Observatory.SotoStarshade import SotoStarshade
+import numpy as np
+import astropy.units as u
+from scipy.integrate import solve_ivp
+import astropy.constants as const
+import hashlib
+import scipy.optimize as optimize
+from scipy.optimize import basinhopping
+import scipy.interpolate as interp
+import scipy.integrate as intg
+from scipy.integrate import solve_bvp
+import matplotlib.pyplot as plt
+from copy import deepcopy
+import time
+import os
+try:
+    import _pickle as pickle
+except:
+    import pickle
+
+EPS = np.finfo(float).eps
+
+
+class SotoStarshade_SK(SotoStarshade):
+    """ StarShade Observatory class
+    This class is implemented at L2 and contains all variables, functions, 
+    and integrators to calculate occulter dynamics. 
+    """
+    
+    def __init__(self,orbit_datapath=None,**specs): 
+
+        SotoStarshade.__init__(self,**specs)  

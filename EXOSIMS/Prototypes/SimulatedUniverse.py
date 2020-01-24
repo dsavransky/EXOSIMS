da20f4ceb6a15259748ae04e636fb3b15bc08f1b
Dean Keithly <drk94@cornell.edu>
2020-01-24 08:30:18 -0500

    adding Python2 compatability2

--- EXOSIMS/Prototypes/SimulatedUniverse.py
+++ EXOSIMS/Prototypes/SimulatedUniverse.py
@@ -273,12 +273,14 @@ class SimulatedUniverse(object):
         
         self.r = (A*r1 + B*r2).T.to('AU')                           # position
         self.v = (v1*(-A*r2 + B*v2)).T.to('AU/day')                 # velocity
-        self.d = np.linalg.norm(self.r, axis=1)                     # planet-star distance
-        self.s = np.linalg.norm(self.r[:,0:2], axis=1)              # apparent separation
         if sys.version_info[0] > 2:
-            self.phi = PPMod.calc_Phi(np.arccos(self.r[:,2]/self.d))    # planet phase
+            self.d = np.linalg.norm(self.r, axis=1)                     # planet-star distance
+            self.s = np.linalg.norm(self.r[:,0:2], axis=1)              # apparent separation
         else:
-            self.phi = PPMod.calc_Phi(np.arccos(self.r[:,2].to('AU').value/self.d.to('AU').value))    # planet phase
+            self.d = np.linalg.norm(self.r, axis=1)*self.r.unit         # planet-star distance
+            self.s = np.linalg.norm(self.r[:,0:2], axis=1)*self.r.unit  # apparent separation
+        
+        self.phi = PPMod.calc_Phi(np.arccos(self.r[:,2]/self.d))    # planet phase
         self.fEZ = ZL.fEZ(TL.MV[self.plan2star], self.I, self.d)    # exozodi brightness
         self.dMag = deltaMag(self.p, self.Rp, self.d, self.phi)     # delta magnitude
         self.WA = np.arctan(self.s/TL.dist[self.plan2star]).to('arcsec')# working angle

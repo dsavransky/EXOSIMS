c381d905bdbd96ae19060625beafeff3e4b4cd5d
Gab <gjsoto3@gmail.com>
2020-04-21 21:48:23 -0400

    latdist for inner and outer as well as axldist are now attributes of sotostarshade

--- EXOSIMS/Observatory/SotoStarshade_SK.py
+++ EXOSIMS/Observatory/SotoStarshade_SK.py
@@ -27,9 +27,13 @@ class SotoStarshade_SK(SotoStarshade_ContThrust):
     and integrators to calculate occulter dynamics. 
     """
     
-    def __init__(self,orbit_datapath=None,**specs): 
+    def __init__(self,latDist=0.9,latDistOuter=0.95,axlDist=250,**specs): 
 
         SotoStarshade_ContThrust.__init__(self,**specs)  
+        
+        self.latDist      = latDist * u.m
+        self.latDistOuter = latDistOuter * u.m
+        self.axlDist      = axlDist * u.km
 
     # converting angular velocity
     def convertAngVel_to_canonical(self,angvel):
@@ -332,8 +336,7 @@ class SotoStarshade_SK(SotoStarshade_ContThrust):
         return distanceFromLim
 
 
-    def drift_event(self,TL,sInd,trajStartTime, latDist = 0.8*u.m, latDistOuter = 0.9*u.m, axDist = 250*u.km,\
-                     dt=20*u.min,neutralStart=True,s0=None,fullSol=False):
+    def drift(self,TL,sInd,trajStartTime,dt=20*u.min,neutralStart=True,s0=None,fullSol=False):
         """
         
         return:
@@ -344,12 +347,12 @@ class SotoStarshade_SK(SotoStarshade_ContThrust):
         EoM = lambda t,s: self.equationsOfMotion_aboutS(t,s,TL,sInd,trajStartTime,integrate=True)
         
         # event function for crossing inner boundary
-        crossInner = lambda t,s: self.crossThreshholdEvent(t,s,TL,sInd,trajStartTime,latDist)
+        crossInner = lambda t,s: self.crossThreshholdEvent(t,s,TL,sInd,trajStartTime,self.latDist)
         crossInner.terminal  = False
         crossInner.direction = 1
         
         # event function for crossing outer boundary
-        crossOuter = lambda t,s: self.crossThreshholdEvent(t,s,TL,sInd,trajStartTime,latDistOuter)
+        crossOuter = lambda t,s: self.crossThreshholdEvent(t,s,TL,sInd,trajStartTime,self.latDistOuter)
         crossOuter.terminal  = True # this one is terminal, should end integration after trigger
         crossOuter.direction = 1
         
@@ -386,7 +389,7 @@ class SotoStarshade_SK(SotoStarshade_ContThrust):
         #(if we trigger in negative y, we should then just resolve the inner boundary crossing instead)
         if t_outerEvent.size > 0 and r_C_outer[1] > 0:
             print('crossed outer')
-            cross = 1
+            cross = 2
             driftTime = self.convertTime_to_dim(t_outerEvent[0]).to('min')
             # rotate states at crossing event to C frame
             r_cross,v_cross = self.rotate_RorC(TL,sInd,trajStartTime,s_outerEvent,t_outerEvent,final_frame='C')
@@ -429,206 +432,52 @@ class SotoStarshade_SK(SotoStarshade_ContThrust):
             return cross, driftTime, t_int, r_full, v_full
         else:
             return cross, driftTime, t_int, r_cross, v_cross
-        
-        
-    def drift(self,TL,sInd,trajStartTime, latDist = 1*u.m, axDist = 250*u.km,\
-                     dt=20*u.min,neutralStart=True,s0=None,fullSol=False):
-        """
-        
-        return:
-            cross - 0,1,or 2 for Tolerance Not Crossed, Lateral Cross, and Axial Cross
-        """
-        
-        EoM = lambda t,s: self.equationsOfMotion_aboutS(t,s,TL,sInd,trajStartTime,integrate=True)
-        
-        # defining times
-        t0 = self.convertTime_to_canonical(np.mod(trajStartTime.value,self.equinox.value)*u.d)[0]
-        tF = t0 + self.convertTime_to_canonical( dt )
-        tInt = np.linspace(0,tF-t0,5000)
-                
-        # remember these states are relative to the nominal track S/0
-        # either start exactly on nominal track or else place it yourself
-        if neutralStart:
-            s0 = np.zeros(6)
-            
-        # integrate forward to t0 + dt
-        res = solve_ivp(EoM,[tInt[0],tInt[-1]],s0,t_eval=tInt,rtol=1e-13,atol=1e-13,method='Radau')
-        t_int = res.t
-        y_int = res.y
-        x,y,z,dx,dy,dz = y_int
-        
-        #gotta rotate trajectory array from R-frame to C-frame components
-        rSpS_C,RdrSpS_C = self.rotate_RorC(TL,sInd,trajStartTime,y_int,t_int,final_frame='C')
-        rSpS_Cdim = self.convertPos_to_dim(rSpS_C).to('m')
-        
-        #### LATERAL OFFSET ########################
-        # lateral distance from nominal point S (in meters)
-        delta_r = rSpS_Cdim[0:2,:]
-        dr = np.linalg.norm(delta_r,axis=0) * u.m
-        
-        # finding where lateral distance is greater than threshhold
-        latCrossInds = np.where( dr >= latDist )[0]
-        
-        if latCrossInds.size > 0:
-            # we crossed the lateral threshhold
-            crossIndL = latCrossInds[0] 
-            cross_Lat   = True
-        else:
-            # we did not cross the lateral threshhold
-            cross_Lat   = False
-        
-        #### AXIAL OFFSET ##########################
-        # axial distance from nominal point S (in meters)
-        dz = rSpS_Cdim[2,:] 
-        
-        # finding where axial distance is greater than threshhold
-        axlCrossInds = np.where( dz >= axDist )[0]
-        
-        if axlCrossInds.size > 0:
-            # we crossed the axial threshhold
-            crossIndA = axlCrossInds[0]
-            cross_Axl   = True
-        else:
-            # we did not cross the axial threshhold
-            cross_Axl = False
-        
-        ### Resolving cross events #################
-        if cross_Axl + cross_Lat == 0:
-            # nothing happened, WE'RE STILL IN THE CIRCLE MURPH
-            cross = 0
-            driftTime = None
-            # just gonna output the last state
-            rSpS_Ccross = rSpS_C[:,-1]
-            RdrSpS_Ccross = RdrSpS_C[:,-1]
-        else:
-            # we did cross the cylinder somewhere (1: laterally, 2:axially)
-            if cross_Axl + cross_Lat == 2:
-                if crossIndL < crossIndA:
-                    cross = 1
-                    crossInd = crossIndL
-                else:
-                    cross = 2
-                    crossInd = crossIndA
-            else:
-                if cross_Lat:
-                    cross = 1
-                    crossInd = crossIndL
-                else:
-                    cross = 2
-                    crossInd = crossIndA
-        
-            #MAKE INTO ITS OWN FUNCTION
-            # actual positions just before and after crossing boundary
-            relevantInds = [crossInd-1,crossInd] #crossInd-1 needs to exist
-            rSpS_C_nearBoundary   = rSpS_C[:,relevantInds]
-            RdrSpS_C_nearBoundary = RdrSpS_C[:,relevantInds]
-            # times just before and after crossing (canonical units)
-            tInterpRange = t_int[relevantInds]
-            # creating initial interpolant, to find a more precise crossing time
-            ri = interp.interp1d( tInterpRange,rSpS_C_nearBoundary )
-            vi = interp.interp1d( tInterpRange,RdrSpS_C_nearBoundary )
-            
-            # interpolate between two relevants points to find better crossing
-            tNew = np.linspace( tInterpRange[0] , tInterpRange[-1], 1000)
-            rNew = ri(tNew) 
-            vNew = vi(tNew)
-            
-            # find new cross time for either crossing event
-            if cross == 1:
-                newDelta_r = self.convertPos_to_dim(rNew[0:2,:]).to('m')
-                drNew = np.linalg.norm(newDelta_r,axis=0) * u.m
-                
-                # finding where lateral distance is greater than threshhold
-                newLatCrossInds = np.where( drNew >= latDist )[0]
-                newCrossInd = newLatCrossInds[0] - 1
-                
-            else:
-                dzNew = self.convertPos_to_dim(rNew[2,:]).to('m')
-                
-                # finding where lateral distance is greater than threshhold
-                newAxlCrossInds = np.where( dzNew >= axDist )[0]
-                newCrossInd = newAxlCrossInds[0] - 1
-            
-            newCrossTime   = tNew[newCrossInd]
-            rSpS_Ccross    = rNew[:,newCrossInd]
-            RdrSpS_Ccross  = vNew[:,newCrossInd]
-            
-            driftTime = self.convertTime_to_dim(newCrossTime).to('min')
-            rSpS_C   = np.hstack([ rSpS_C[:,:crossInd] , rSpS_Ccross.reshape(3,1)])
-            RdrSpS_C = np.hstack([ RdrSpS_C[:,:crossInd] , RdrSpS_Ccross.reshape(3,1)])
-        
-        
-        if fullSol:
-            return cross, driftTime, t_int, rSpS_C, RdrSpS_C
-        else:
-            return cross, driftTime, t_int, rSpS_Ccross, RdrSpS_Ccross
-                
 
-    def guessAParabola(self,TL,sInd,trajStartTime,rSpS_C,RdrSpS_C,cross,\
-                       latDist = 1*u.m, axDist = 250*u.km):
+
+    def guessAParabola(self,TL,sInd,trajStartTime,rSpS_C,RdrSpS_C,cross,latDist = 0.9*u.m,fullSol=False):
         
         
         Ra_S0_R , f_S0_R, fA_B, fL_B, theta = self.starshadeAcceleration(TL,sInd,trajStartTime)
 
-        if cross == 1:
-            a = np.linalg.norm(fL_B)
-            DU = self.convertPos_to_canonical(latDist)
-            VU = np.sqrt( a*DU )
-            TU = np.sqrt( DU/a )
+
+        a = np.linalg.norm(fL_B)
+        DU = self.convertPos_to_canonical(latDist)
+        VU = np.sqrt( a*DU )
+        TU = np.sqrt( DU/a )
+    
+        # converting initial state from canonical units to C-frame units
+        x0,y0,z0       = rSpS_C[:,-1] / DU
+        dx0_,dy0_,dz0_ = RdrSpS_C[:,-1] / VU
+    
         
-            # converting initial state from canonical units to C-frame units
-            x0,y0,z0       = rSpS_C[:,-1] / DU
-            dx0_,dy0_,dz0_ = RdrSpS_C[:,-1] / VU
-        
-            
-            # finding intercept point
-            if y0 > 0.5:
-                xi = x0
-                yi = y0
-            else:
-                xi = np.sqrt(1+y0)/np.sqrt(2) if x0 > 0 else -np.sqrt(1+y0)/np.sqrt(2)
-                yi = np.sqrt(1-y0)/np.sqrt(2)
-            
-            # initial velocities of the second parabola
-            dx0 = np.sqrt(yi*(1-yi))/np.sqrt(2) if x0 < 0 else -np.sqrt(yi*(1-yi))/np.sqrt(2)
-            dy0 = -dx0*xi/yi + (xi-x0)/dx0
-            
-            # time of flight in C-frame units
-            tof = np.abs( x0 / dx0 ) if x0 != 0 else 4
-            
-            ### planar parabolic trajectories in C-frame units
-            # time range from 0 to full time of flight
-            t = np.linspace(0,tof,1000)
-            
-            # x and y throughout entire new parabolic trajectory
-            xP  = dx0*t + x0
-            yP  = -0.5*t**2 + dy0*t + y0
-            
-            parab   = np.vstack([xP,yP])  * DU
-            RdrSpS_C_newIC = np.array([dx0,dy0,dz0_]) * VU
-            dt_newTOF = tof * TU
-            
+        # finding intercept point
+        if y0 > 0.5:
+            xi = x0
+            yi = y0
         else:
-            a = np.abs(fA_B[0])
-            DU = self.convertPos_to_canonical(axDist)
-            VU = np.sqrt( a*DU )
-            TU = np.sqrt( DU/a )
+            xi = np.sqrt(1+y0)/np.sqrt(2) if x0 > 0 else -np.sqrt(1+y0)/np.sqrt(2)
+            yi = np.sqrt(1-y0)/np.sqrt(2)
+            
+        # initial velocities of the second parabola
+        dx0 = np.sqrt(yi*(1-yi))/np.sqrt(2) if x0 < 0 else -np.sqrt(yi*(1-yi))/np.sqrt(2)
+        dy0 = -dx0*xi/yi + (xi-x0)/dx0
         
-            # converting initial state from canonical units to C-frame units
-            x0,y0,z0       = rSpS_C[:,-1] / DU
-            dx0_,dy0_,dz0_ = RdrSpS_C[:,-1] / VU
-            
-            sgnA = np.sign(fA_B[0])
-            sgnV = np.sign(dz0_)
-            sgnD = np.sign(z0)
-            
-            dz0 = -sgnV * np.sqrt( 4 * np.abs(z0) ) if sgnD * sgnA == 1 else 0
-            tof = 2 * np.sqrt( 4 * np.abs(z0) )
-            
-            t = np.linspace(0,tof,1000)
-            
-            parab = (sgnA * t**2 + dz0 * t + z0) * DU
-            RdrSpS_C_newIC = np.array([dx0_,dy0_,dz0]) * VU
-            dt_newTOF = tof * TU
+        # time of flight in C-frame units
+        tof = np.abs( x0 / dx0 ) if x0 != 0 else 4
         
-        return dt_newTOF, RdrSpS_C_newIC, parab
+        ### planar parabolic trajectories in C-frame units
+        # time range from 0 to full time of flight
+        t = np.linspace(0,tof,1000)
+        
+        # x and y throughout entire new parabolic trajectory
+        xP  = dx0*t + x0
+        yP  = -0.5*t**2 + dy0*t + y0
+        
+        parab   = np.vstack([xP,yP])  * DU
+        RdrSpS_C_newIC = np.array([dx0,dy0,dz0_]) * VU
+        dt_newTOF = tof * TU
+        
+        if fullSol:
+            return dt_newTOF, RdrSpS_C_newIC, parab
+        else:
+            return dt_newTOF, RdrSpS_C_newIC

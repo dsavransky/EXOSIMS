93d45df52fa0116726a3ef575b34eb5833ecf0a2
wbalmer <williamobalmer@gmail.com>
2020-07-28 14:19:54 -0400

    F0, mag tuple into Nemati optical system

--- EXOSIMS/OpticalSystem/Nemati.py
+++ EXOSIMS/OpticalSystem/Nemati.py
@@ -8,25 +8,25 @@ from numpy import nan
 
 class Nemati(OpticalSystem):
     """Nemati Optical System class
-    
+
     This class contains all variables and methods necessary to perform
     Optical System Module calculations in exoplanet mission simulation using
     the model from Nemati 2014.
-    
+
     Args:
         specs:
             user specified values
-    
+
     """
-    
+
     def __init__(self, **specs):
-        
+
         OpticalSystem.__init__(self, **specs)
 
     def calc_intTime(self, TL, sInds, fZ, fEZ, dMag, WA, mode, TK=None):
-        """Finds integration times of target systems for a specific observing 
+        """Finds integration times of target systems for a specific observing
         mode (imaging or characterization), based on Nemati 2014 (SPIE).
-        
+
         Args:
             TL (TargetList module):
                 TargetList class object
@@ -45,16 +45,16 @@ class Nemati(OpticalSystem):
             TK (TimeKeeping object):
                 Optional TimeKeeping object (default None), used to model detector
                 degradation effects where applicable.
-        
+
         Returns:
             intTime (astropy Quantity array):
                 Integration times in units of day
-        
+
         """
-        
+
         # electron counts
         C_p, C_b, C_sp = self.Cp_Cb_Csp(TL, sInds, fZ, fEZ, dMag, WA, mode, TK=TK)
-        
+
         # get SNR threshold
         SNR = mode['SNR']
         # calculate integration time based on Nemati 2014
@@ -67,13 +67,13 @@ class Nemati(OpticalSystem):
         intTime[np.isinf(intTime) | np.isnan(intTime)] = 0.*u.d
         # negative values are set to zero
         intTime[intTime.value < 0.] = 0.*u.d
-        
+
         return intTime.to('day')
 
     def calc_dMag_per_intTime(self, intTimes, TL, sInds, fZ, fEZ, WA, mode, C_b=None, C_sp=None, TK=None):
-        """Finds achievable dMag for one integration time per star in the input 
+        """Finds achievable dMag for one integration time per star in the input
         list at one working angle.
-        
+
         Args:
             intTimes (astropy Quantity array):
                 Integration times
@@ -99,13 +99,13 @@ class Nemati(OpticalSystem):
             TK (TimeKeeping object):
                 Optional TimeKeeping object (default None), used to model detector
                 degradation effects where applicable.
-            
+
         Returns:
             dMag (ndarray):
                 Achievable dMag for given integration time and working angle
-                
+
         """
-        
+
         # cast sInds, WA, fZ, fEZ, and intTimes to arrays
         sInds = np.array(sInds, ndmin=1, copy=False)
         WA = np.array(WA.value, ndmin=1)*WA.unit
@@ -120,28 +120,26 @@ class Nemati(OpticalSystem):
         # get scienceInstrument and starlightSuppressionSystem
         inst = mode['inst']
         syst = mode['syst']
-        
+
         # get mode wavelength
         lam = mode['lam']
         # get mode fractional bandwidth
         BW = mode['BW']
         # get mode bandwidth (including any IFS spectral resolving power)
         deltaLam = lam/inst['Rs'] if 'spec' in inst['name'].lower() else mode['deltaLam']
-        
-        # get star magnitude
-        mV = TL.starMag(sInds, lam)
-        
+
+
         # get signal to noise ratio
         SNR = mode['SNR']
-        
+
         # spectral flux density = F0 * A * Dlam * QE * T (attenuation due to optics)
         attenuation = inst['optics']*syst['optics']
-        F_0 = TL.starF0(sInds,mode)
+        F_0, mag = TL.starF0(sInds,mode)
         C_F0 = F_0*self.pupilArea*deltaLam*inst['QE'](lam)*attenuation
-        
+
         # get core_thruput
         core_thruput = syst['core_thruput'](lam, WA)
-        
+
         # calculate planet delta magnitude
         dMagLim = np.zeros(len(sInds)) + TL.Completeness.dMagLim
         if (C_b is None) or (C_sp is None):
@@ -149,14 +147,14 @@ class Nemati(OpticalSystem):
         intTimes[intTimes.value < 0.] = 0.
         tmp = np.nan_to_num(C_b/intTimes)
         assert all(tmp + C_sp**2. >= 0.) , 'Invalid value in Nemati sqrt, '
-        dMag = -2.5*np.log10((SNR*np.sqrt(tmp + C_sp**2.)/(C_F0*10.0**(-0.4*mV)*core_thruput*inst['PCeff'])).decompose().value)
+        dMag = -2.5*np.log10((SNR*np.sqrt(tmp + C_sp**2.)/(C_F0*10.0**(-0.4*mag)*core_thruput*inst['PCeff'])).decompose().value)
         dMag[np.where(np.isnan(dMag))[0]] = 0. # this is an error catch. if intTimes = 0, the dMag becomes infinite
 
         return dMag
 
     def ddMag_dt(self, intTimes, TL, sInds, fZ, fEZ, WA, mode, C_b=None, C_sp=None, TK=None):
         """Finds derivative of achievable dMag with respect to integration time
-        
+
         Args:
             intTimes (astropy Quantity array):
                 Integration times
@@ -182,13 +180,13 @@ class Nemati(OpticalSystem):
             TK (TimeKeeping object):
                 Optional TimeKeeping object (default None), used to model detector
                 degradation effects where applicable.
-            
+
         Returns:
             ddMagdt (ndarray):
                 Derivative of achievable dMag with respect to integration time
-        
+
         """
-        
+
         # cast sInds, WA, fZ, fEZ, and intTimes to arrays
         sInds = np.array(sInds, ndmin=1, copy=False)
         WA = np.array(WA.value, ndmin=1)*WA.unit
@@ -199,10 +197,10 @@ class Nemati(OpticalSystem):
         assert len(fEZ) == len(sInds), "fEZ must be an array of length len(sInds)"
         assert len(fZ) == len(sInds), "fZ must be an array of length len(sInds)"
         assert len(WA) == len(sInds), "WA must be an array of length len(sInds)"
-        
+
         dMagLim = np.zeros(len(sInds)) + 25.
         if (C_b is None) or (C_sp is None):
             _, C_b, C_sp = self.Cp_Cb_Csp(TL, sInds, fZ, fEZ, dMagLim, WA, mode, TK=TK)
         ddMagdt = 2.5/(2.0*np.log(10.0))*(C_b/(C_b*intTimes + (C_sp*intTimes)**2.)).to('1/s').value
-        
+
         return ddMagdt/u.s

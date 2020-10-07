a672f994c56c4c2f7e8f0b69a71e3f4377429ae6
Dmitry Savransky <dsavransky@gmail.com>
2020-10-07 14:01:26 -0400

    adding pickles library to mainfest and bumping version to 2.1.0

--- EXOSIMS/__init__.py
+++ EXOSIMS/__init__.py
@@ -1,5 +1,5 @@
 name = "EXOSIMS"
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 # Set up a default logging handler to avoid "No handler found" warnings.
 # Other handlers can add to this one.
 import logging

ff921c152cbd084594cddf09e6508554305c7b00
Dean Keithly <drk94@cornell.edu>
2020-11-15 08:52:33 -0500

    adding planet-star separation function to utils

---
+++ EXOSIMS/util/planet_star_separation.py
@@ -0,0 +1,19 @@
+# Planet Star Separation
+# Written By: Dean Keithly
+# Written On: 11/13/2020
+
+def planet_star_separation(a,e,v,w,i):
+    """ Following directly from Keithly 2021. Calculates planet star separation given KOE
+    Args:
+        a (numpy array):
+        e (numpy array):
+        v (numpy array):
+        w (numpy array):
+        i (numpy array):
+    Returns:
+        s (numpy array):
+            planet-star separations in AU
+    """
+    r = a*(1.-e**2.)/(1.+e*np.cos(v))
+    s = r/np.sqrt(2.)*np.sqrt(1+np.cos(i)**2. + (1.-np.cos(i)**2)*np.cos(2.*(w+v)))
+    return s

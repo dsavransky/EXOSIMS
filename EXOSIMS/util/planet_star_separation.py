a06e293da856ea5631508e3968d70944b86ddabe
Dean Keithly <drk94@cornell.edu>
2021-02-19 15:31:46 -0500

    changing header format

--- EXOSIMS/util/planet_star_separation.py
+++ EXOSIMS/util/planet_star_separation.py
@@ -1,22 +1,27 @@
-# Planet Star Separation
-# Written By: Dean Keithly
-# Written On: 11/13/2020
+"""
+Planet Star Separation
+Written By: Dean Keithly
+Written On: 11/13/2020
+"""
 import numpy as np
 
 def planet_star_separation(a,e,v,w,i):
     """ Following directly from Keithly 2021. Calculates planet star separation given KOE
     Args:
-        a (numpy array): planet semi-major axis in AU
-        e (numpy array): planet eccentricity
-        v (numpy array): planet true anomaly rad
-        w (numpy array): planet argument of periapsis rad
-        i (numpy array): planet inclination rad
+        a (numpy array):
+            planet semi-major axis in AU
+        e (numpy array):
+            planet eccentricity
+        v (numpy array):
+            planet true anomaly rad
+        w (numpy array):
+            planet argument of periapsis rad
+        i (numpy array):
+            planet inclination rad
     Returns:
         s (numpy array):
             planet-star separations in AU
     """
     r = a*(1.-e**2.)/(1.+e*np.cos(v))
-    #I couldn't find where this next equation is
-    #s = r/np.sqrt(2.)*np.sqrt(1+np.cos(i)**2. + (1.-np.cos(i)**2)*np.cos(2.*(w+v)))
-    s = r*np.sqrt(np.sin(v+w)**2.*np.cos(i)**2 + np.cos(v+w)**2.) #this is from threeDellipseToTwoDellipse
+    s = r*np.sqrt(np.sin(v+w)**2.*np.cos(i)**2 + np.cos(v+w)**2.) #this is from threeDellipseToTwoDellipse.ipynb
     return s

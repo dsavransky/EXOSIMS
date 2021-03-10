4ebe77307b95e44f173cfe7b6d6c22e2d567b6c2
Dmitry Savransky <dsavransky@gmail.com>
2021-03-10 11:22:13 -0500

    Update __init__.py
    version bump

--- EXOSIMS/__init__.py
+++ EXOSIMS/__init__.py
@@ -1,5 +1,5 @@
 name = "EXOSIMS"
-__version__ = '2.1.2'
+__version__ = '2.1.3'
 # Set up a default logging handler to avoid "No handler found" warnings.
 # Other handlers can add to this one.
 import logging

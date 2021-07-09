983bc94accd28d5a55b97dbb1bfc4f0cf6e8e1cc
Sonny Rappaport <str36@cornell.edu>
2021-07-09 19:01:23 -0400

    move phaseFunctions back to this branch

--- tests/util/test_phaseFunctions.py
+++ tests/util/test_phaseFunctions.py
@@ -3,7 +3,9 @@ import unittest
 from astropy import units as u
 from astropy import constants as const
 import EXOSIMS.util.phaseFunctions as pf
+import EXOSIMS.PlanetPhysicalModel as ppm
 import numpy as np
+import math
 
 """phaseFunctions.py module unit tests
 
@@ -20,20 +22,17 @@ class TestPhaseFunctions(unittest.TestCase):
           Note: Inverse should only take on input values from 0 to 1.
           """
 
-        i1 = np.array([0,.1,.2,.3,.4])
+        i1 = np.array([0,.1,.2,.3,.4,.5,.6,.7,.8])
         result = pf.quasiLambertPhaseFunction(i1)
-        expected = np.array([1.,0.9950,0.9802,0.9558,0.9226])
-        np.testing.assert_allclose(expected, result, rtol=1e-1, atol=0)
+        expected = np.array([1.0, 0.9950104048691683, 0.9801659131709817, 0.9558351964265126, 0.9226188356698382, 0.8813290691787037, 0.8329625267644232, 0.7786669865047742, 0.7197034143859217])
+        np.testing.assert_allclose(expected, result, rtol=1e-8, atol=0)
 
-        i2 = np.array([1.,0.9950,0.9802,0.9558,0.9226])
+        i2 = np.array([1.0, 0.9950104048691683, 0.9801659131709817, 0.9558351964265126, 0.9226188356698382, 0.8813290691787037, 0.8329625267644232, 0.7786669865047742, 0.7197034143859217])
         result2 = pf.quasiLambertPhaseFunctionInverse(i2)
-        expected2 = np.array([0,.1,.2,.3,.4]) 
-        np.testing.assert_allclose(expected2, result2, rtol=1e-1, atol=0)
+        expected2 = np.array([0,.1,.2,.3,.4,.5,.6,.7,.8]) 
+        np.testing.assert_allclose(expected2, result2, rtol=1e-8, atol=0)
 
     def test2(self): 
-        """ Testing the hyperbolicTangentPhaseFunc.""" 
-
-    def test3(self): 
         """ Testing the phi_lambert function for arbitrary small inputs.
         Numbers calculated on desmos. 
         """ 
@@ -41,21 +40,72 @@ class TestPhaseFunctions(unittest.TestCase):
         expected = [1,.995,.981,.9582,.9277,.8910,.8474,.7995,.7476,.6929,.6362]
 
         for x in np.arange(0,11,1): 
-          self.assertAlmostEqual (pf.phi_lambert(x/10),expected[x],delta=1e-2)
+          self.assertAlmostEqual (pf.phi_lambert(x/10),expected[x],delta=1e-8)
 
-    def test4(self): 
+    def test3(self): 
         """ Testing the transitionStart and transitionEnd functions, specifically
-        testing for the midpoint value and two extreme values. """ 
+        testing for the midpoint value and two extreme values. 
+        
+        Also checks that the other phase functions begin at 0 degrees around 
+        1 and ends at 180 around 180.
 
-        self.assertAlmostEqual(pf.transitionStart(-1e100,0,1),0,delta=.001)
-        self.assertAlmostEqual(pf.transitionStart(1e100,0,1),1,delta=.001)
-        self.assertAlmostEqual(pf.transitionStart(0,0,1),.5,delta=.001)
-        self.assertAlmostEqual(pf.transitionStart(2,2,1),.5,delta=.001)
+        The hyperbolic tangent function is given somewhat looser bounds as it's
+        a tanh function. 
+        """ 
 
-        self.assertAlmostEqual(pf.transitionEnd(-1e100,0,1),1,delta=.001)
-        self.assertAlmostEqual(pf.transitionEnd(1e100,0,1),0,delta=.001)
-        self.assertAlmostEqual(pf.transitionEnd(0,0,1),.5,delta=.001)
-        self.assertAlmostEqual(pf.transitionEnd(2,2,1),.5,delta=.001)
+        delta1 = 1e-8
+        delta2 = 1e-2 
+        #looser delta 
+
+        self.assertAlmostEqual(pf.transitionStart(-1e100,0,1),0,delta=delta1)
+        self.assertAlmostEqual(pf.transitionStart(1e100,0,1),1,delta=delta1)
+        self.assertAlmostEqual(pf.transitionStart(0,0,1),.5,delta=delta1)
+        self.assertAlmostEqual(pf.transitionStart(2,2,1),.5,delta=delta1)
+        #transition start
+
+        self.assertAlmostEqual(pf.transitionEnd(-1e100,0,1),1,delta=delta1)
+        self.assertAlmostEqual(pf.transitionEnd(1e100,0,1),0,delta=delta1)
+        self.assertAlmostEqual(pf.transitionEnd(0,0,1),.5,delta=delta1)
+        self.assertAlmostEqual(pf.transitionEnd(2,2,1),.5,delta=delta1)
+        #transition end
+
+        self.assertAlmostEqual(pf.phi_lambert(0),1,delta=delta1)
+        self.assertAlmostEqual(pf.phi_lambert(180),0,delta=delta1)
+        #phi_lampert
+
+        self.assertAlmostEqual(pf.quasiLambertPhaseFunction(0),1,delta=delta1)
+        self.assertAlmostEqual(pf.quasiLambertPhaseFunction(180),0,delta=delta1)
+        #quasi_lampert phase function
+
+
+        self.assertAlmostEqual(pf.hyperbolicTangentPhaseFunc(0,0,0,0,0,'mercury'),1,delta=delta2)
+        self.assertAlmostEqual(pf.hyperbolicTangentPhaseFunc(180,0,0,0,0,'mercury'),0,delta=delta2)
+        self.assertAlmostEqual(pf.hyperbolicTangentPhaseFunc(-1e100,0,1),1,delta=delta2)
+        self.assertAlmostEqual(pf.hyperbolicTangentPhaseFunc(1e100,0,1),0,delta=delta2)
+        self.assertAlmostEqual(pf.hyperbolicTangentPhaseFunc(-1e100,0,1),1,delta=delta2)
+        self.assertAlmostEqual(pf.hyperbolicTangentPhaseFunc(1e100,0,1),0,delta=delta2)
+        #hyperbolicTangentPhaseFunc for each planet 
+
+    def test4(self):
+        """ Testing the hyperbolicTangentPhaseFunc for each of the predefined
+        inputs (the various planets.) Uses the functions from Mallama2018 using
+        some of their sample input/outputs. Uses a loose bound for the test 
+        as average values are used.
+        
+        For each of the nested functions:
+
+        args: 
+            r (float): Planet's distance from the sun.
+            d (float): Planet's distance from the earth.
+            a (float): Planet phase angle in degrees. Requires 0 < a < 180. 
+        
+        Returns: apparant magnitude, as described via mallama2018's various 
+        functions for the different planets.
+        """
+
+        self.assertAlmostEqual(pf.(mercAM(0.413629222334,0.92644808718613,90.1662)+2,90,5000),pf.hyperbolicTangentPhaseFunc(90.1662*u.deg,0,0,0,0,planetName='mercury'),delta=.01)
+        self.assertAlmostEqual(venusAM(0.721480714554,0.37762511206278,124.1348),pf.hyperbolicTangentPhaseFunc(124.1348*u.deg,0,0,0,0,planetName='venus'),delta=.01)
+
           
 
 if __name__ == '__main__':

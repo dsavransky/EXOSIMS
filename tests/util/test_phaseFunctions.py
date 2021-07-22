fix very small computational precision error

tests/util/test_phaseFunctions.py
@@ -153,7 +153,7 @@ class TestPhaseFunctions(unittest.TestCase):
 
         input = np.array([0,.1,.2,.3,.4,.5,.6,.7,.8])
 
-        np.testing.assert_array_equal(pf.betaFunc(input,input,input),[1.5707963267948966, 1.5509611881121566, 1.4933534592588231, 1.4031488617499241, 1.28767754720778, 1.1555419735883117, 1.0165896898819988, 0.8829895396912839, 0.7712352623707003])
+        np.testing.assert_allclose(pf.betaFunc(input,input,input),[1.5707963267948966, 1.5509611881121566, 1.4933534592588231, 1.4031488617499241, 1.28767754720778, 1.1555419735883117, 1.0165896898819988, 0.8829895396912839, 0.7712352623707003])
         #test array of inputs 
         self.assertEqual(pf.betaFunc(.1,.1,.1),1.5509611881121566)
         #test a singular input
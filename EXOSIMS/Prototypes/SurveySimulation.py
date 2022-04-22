Fixed color correction calculation in Stark.fZ

EXOSIMS/Prototypes/SurveySimulation.py
@@ -398,17 +398,17 @@ class SurveySimulation(object):
                 self.koMaps[n] = koMaps[x,:,:]
         
         if not(nofZ):
-            self.ZodiacalLight.fZ_startSaved = {}
+            self.ZodiacalLight.fZMap = {}
             self.fZQuads = {}
             for x,n in zip(systOrder,systNames[systOrder]):
-                self.ZodiacalLight.fZ_startSaved[n] = np.array([])
+                self.ZodiacalLight.fZMap[n] = np.array([])
                 self.fZQuads[n] = np.array([])
-        # need to make fZ_startSaved and fZQuads (and maybe valfZmin and absTimefZmin) like self.koMaps
+        # need to make fZMap and fZQuads (and maybe valfZmin and absTimefZmin) like self.koMaps
 
         # Precalculating intTimeFilter
         sInds = np.arange(TL.nStars) #Initialize some sInds array
         modeHashName = self.cachefname[0:-2]+'_'+self.mode['syst']['name']+'.'
-        self.ZodiacalLight.fZ_startSaved[self.mode['syst']['name']] = self.ZodiacalLight.generate_fZ(self.Observatory, TL, self.TimeKeeping, self.mode, modeHashName)
+        self.ZodiacalLight.fZMap[self.mode['syst']['name']] = self.ZodiacalLight.generate_fZ(self.Observatory, TL, self.TimeKeeping, self.mode, modeHashName)
         koMap = self.koMaps[self.mode['syst']['name']]
         self.fZQuads[self.mode['syst']['name']] = self.ZodiacalLight.calcfZmin(sInds, self.Observatory, TL, self.TimeKeeping, self.mode, modeHashName, koMap, self.koTimes) # find fZmin to use in intTimeFilter
         self.valfZmin, self.absTimefZmin = self.ZodiacalLight.extractfZmin_fZQuads(self.fZQuads[self.mode['syst']['name']])

EXOSIMS/Prototypes/TargetList.py
@@ -26,7 +26,7 @@ except:
     import urllib
 import pkg_resources
 import sys
-
+import pdb
 class TargetList(object):
     """Target List class template
     
@@ -390,7 +390,7 @@ class TargetList(object):
             astropy Quantity:
                 Spectral flux density in units of ph/m**2/s/nm.
         """
-        
+#        pdb.set_trace()
         if spec is not None:
             # Try to decmompose the input spectral type
             tmp = self.specregex1.match(spec)
@@ -441,7 +441,7 @@ class TargetList(object):
             ls = sdat.WAVELENGTH[band]*u.Angstrom
             Fs = (sdat.FLUX[band]*u.erg/u.s/u.cm**2/u.AA)*(ls/const.h/const.c)
             F0 = (np.sum((Fs[1:]+Fs[:-1])*np.diff(ls)/2.)/(lmax-lmin)*u.ph).to(u.ph/u.s/u.m**2/u.nm)
-                
+#        pdb.set_trace()
         return F0
 
     
@@ -981,7 +981,7 @@ class TargetList(object):
                 Spectral flux densities in units of ph/m**2/s/nm.
         
         """
-
+#        pdb.set_trace()
         if mode['hex'] in self.F0dict:
             tmp = np.isnan(self.F0dict[mode['hex']][sInds])
             if np.any(tmp):

EXOSIMS/Prototypes/ZodiacalLight.py
@@ -10,7 +10,7 @@ except:
     import pickle
 import sys
 from astropy.time import Time
-
+import pdb
 # Python 3 compatibility:
 if sys.version_info[0] > 2:
     xrange = range
@@ -65,8 +65,22 @@ class ZodiacalLight(object):
         path = os.path.dirname(os.path.abspath(__file__)) + '/../ZodiacalLight'
         Izod = np.loadtxt(os.path.join(path, 'Leinert98_table17.txt'))*1e-8 # W/m2/sr/um
         z = Izod/Izod[12,0]
-        self.fZminglobal = np.min(z)/u.arcsec**2
         
+        
+#        pdb.set_trace()
+#        lam = mode['lam'] # extract wavelength
+#
+#        f = 10.**(self.logf(np.log10(lam.to('um').value)))*u.W/u.m**2/u.sr/u.um
+#        h = const.h                             # Planck constant
+#        c = const.c                             # speed of light in vacuum
+#        ephoton = h*c/lam/u.ph                  # energy of a photon
+#
+#        F0 = TL.OpticalSystem.F0(lam)           # zero-magnitude star (in ph/s/m2/nm)
+#        f_corr = f/ephoton/F0                   # color correction factor
+#        pdb.set_trace()
+#        self.fZminglobal = np.min(z)*f_corr.to('1/arcsec2') # global minimum local zodi brightness
+        self.fZminglobal = np.min(z)/u.arcsec**2
+#        pdb.set_trace()
         assert self.varEZ >= 0, "Exozodi variation must be >= 0"
         
         #### Common Star System Number of Exo-zodi
@@ -207,7 +221,7 @@ class ZodiacalLight(object):
                 hashname describing the files specific to the current json script
                 
         Updates Attributes:
-            fZ_startSaved[1000, TL.nStars] (astropy Quantity array):
+            fZMap[1000, TL.nStars] (astropy Quantity array):
                 Surface brightness of zodiacal light in units of 1/arcsec2 for each star over 1 year at discrete points defined by resolution
         """
         
@@ -342,10 +356,10 @@ class ZodiacalLight(object):
             absTimefZmin = nZ*u.d + TK.currentTimeAbs
 
 
-#            if not hasattr(self,'fZ_startSaved'):
-#                self.fZ_startSaved = self.generate_fZ(Obs, TL, TK, mode, hashname)
-            tmpfZ = np.asarray(self.fZ_startSaved)
-            fZ_matrix = tmpfZ[sInds,:]#Apply previous filters to fZ_startSaved[sInds, 1000]
+#            if not hasattr(self,'fZMap'):
+#                self.fZMap = self.generate_fZ(Obs, TL, TK, mode, hashname)
+            tmpfZ = np.asarray(self.fZMap)
+            fZ_matrix = tmpfZ[sInds,:]#Apply previous filters to fZMap[sInds, 1000]
             dt = 365.25/len(np.arange(1000))
             timeArray = [j*dt for j in np.arange(1000)]
             timeArrayAbs = TK.currentTimeAbs + timeArray*u.d
@@ -420,13 +434,17 @@ class ZodiacalLight(object):
                 if fZQuads[i][j][1].value < ffZmin:
                     ffZmin = fZQuads[i][j][1].value
                     fabsTimefZmin = fZQuads[i][j][3].value
+                    
+            if len(fZQuads[i]) == 0:
+                ffZmin = np.nan
+                fabsTimefZmin = -1
+
             valfZmin.append(ffZmin)
             absTimefZmin.append(fabsTimefZmin)
-        #ADD AN ASSERT CHECK TO ENSURE NO FFZMIN=100 AND NO FABSTIMEFZMIN=0.
-        #The np.asarray and Time must occur to create astropy Quantity arrays and astropy Time arrays
-        # for i in np.arange(len(fZQuads)):
-        #     valfZmin = fZQuads[i][1]
-        #     absTimefZmin = fZQuads[i][3]
-        # return np.asarray(valfZmin), np.asarray(absTimefZmin)
+
+            assert ffZmin != 100., "fZmin not below 100 counts/arcsec^2"
+
+            assert fabsTimefZmin != 0., "absTimefZmin is 0 days"
+
         return np.asarray(valfZmin)/u.arcsec**2., Time(np.asarray(absTimefZmin),format='mjd',scale='tai')
 

EXOSIMS/SurveySimulation/starkAYO_staticSchedule.py
@@ -189,13 +189,13 @@ class starkAYO_staticSchedule(SurveySimulation):
         TK = self.TimeKeeping
 
         #indexFrac = np.interp((TK.currentTimeNorm).value%365.25,[0,365.25],[0,1000])#float from 0 to 1000 of where at in 1 year
-        #tmp = np.asarray(ZL.fZ_startSaved)[self.schedule,:]
+        #tmp = np.asarray(ZL.fZMap)[self.schedule,:]
         #fZ_matrixSched = (indexFrac%1)*tmp[:,int(indexFrac)] + (1-indexFrac%1)*tmp[:,int(indexFrac%1+1)]#A simple interpolant
 
         #fZ_matrixSched = ZL.fZ(Obs, TL, sInds, TK.currentTimeAbs, self.mode)
         fZ_matrixSched = np.zeros(TL.nStars)
         fZ_matrixSched[sInds] = ZL.fZ(Obs, TL, sInds, TK.currentTimeAbs, self.mode)
-        #fZ_matrixSched = np.asarray(ZL.fZ_startSaved)[self.schedule,indexFrac]#has shape [self.schedule.shape[0], 1000]
+        #fZ_matrixSched = np.asarray(ZL.fZMap)[self.schedule,indexFrac]#has shape [self.schedule.shape[0], 1000]
         #The above line might not work because it must be filtered down one index at a time...
         fZminSched = np.zeros(TL.nStars)
         fZminSched[sInds] = self.valfZmin[sInds].value #has shape [self.schedule.shape[0]]

EXOSIMS/SurveySimulation/tieredScheduler.py
@@ -183,7 +183,7 @@ class tieredScheduler(SurveySimulation):
         sInds = np.arange(TL.nStars) #Initialize some sInds array
         #ORIGINALself.occ_valfZmin, self.occ_absTimefZmin = self.ZodiacalLight.calcfZmin(sInds, self.Observatory, TL, self.TimeKeeping, char_mode, self.cachefname) # find fZmin to use in intTimeFilter
         modeHashName = self.cachefname[0:-2]+'_'+char_mode['syst']['name']+'.'
-        self.ZodiacalLight.fZ_startSaved[char_mode['syst']['name']] = self.ZodiacalLight.generate_fZ(self.Observatory, TL, self.TimeKeeping, char_mode, modeHashName)
+        self.ZodiacalLight.fZMap[char_mode['syst']['name']] = self.ZodiacalLight.generate_fZ(self.Observatory, TL, self.TimeKeeping, char_mode, modeHashName)
         koMap = self.koMaps[char_mode['syst']['name']]
         self.fZQuads[char_mode['syst']['name']] = self.ZodiacalLight.calcfZmin(sInds, self.Observatory, TL, self.TimeKeeping, char_mode, modeHashName, koMap, self.koTimes) # find fZmin to use in intTimeFilter
         self.occ_valfZmin, self.occ_absTimefZmin = self.ZodiacalLight.extractfZmin_fZQuads(self.fZQuads[char_mode['syst']['name']])

EXOSIMS/SurveySimulation/tieredScheduler_SLSQP.py
@@ -69,7 +69,7 @@ class tieredScheduler_SLSQP(SLSQPScheduler):
                  revisit_weight=1.0, GAPortion=.25, int_inflection=False,
                  GA_simult_det_fraction=.07, promote_hz_stars=False, phase1_end=365, 
                  n_det_remove=3, n_det_min=3, occ_max_visits=3, max_successful_chars=1,
-                 max_successful_dets=4, nmax_promo_det=4, lum_exp=1, **specs):
+                 max_successful_dets=4, nmax_promo_det=4, lum_exp=1, nofZ=False, **specs):
         
         SLSQPScheduler.__init__(self, **specs)
         
@@ -159,18 +159,27 @@ class tieredScheduler_SLSQP(SLSQPScheduler):
         self.known_earths = np.array([])   # list of detected earth-like planets aroung promoted stars
         self.ignore_stars = []       # list of stars that have been removed from the occ_sInd list
  
+         if not(nofZ):
+            self.ZodiacalLight.fZMap = {}
+            self.fZQuads = {}
+            for x,n in zip(systOrder,systNames[systOrder]):
+                self.ZodiacalLight.fZMap[n] = np.array([])
+                self.fZQuads[n] = np.array([])
+        # need to make fZMap and fZQuads (and maybe valfZmin and absTimefZmin) like self.koMaps
+ 
         # Precalculating intTimeFilter
         allModes = OS.observingModes
         char_mode = list(filter(lambda mode: 'spec' in mode['inst']['name'], allModes))[0]
         sInds = np.arange(TL.nStars) #Initialize some sInds array
         #ORIGINAL self.occ_valfZmin, self.occ_absTimefZmin = self.ZodiacalLight.calcfZmin(sInds, self.Observatory, TL, self.TimeKeeping, char_mode, self.cachefname) # find fZmin to use in intTimeFilter
-        koMap = self.koMaps[char_mode['syst']['name']]
-        self.fZQuads = self.ZodiacalLight.calcfZmin(sInds, self.Observatory, TL, self.TimeKeeping, char_mode, self.cachefname, koMap, self.koTimes) # find fZmin to use in intTimeFilter
-        self.occ_valfZmin, self.occ_absTimefZmin = self.ZodiacalLight.extractfZmin_fZQuads(self.fZQuads)
+        modeHashName = self.cachefname[0:-2]+'_'+char_mode['syst']['name']+'.'
+        koMap[char_mode['syst']['name']] = self.koMaps[char_mode['syst']['name']]
+        self.fZQuads[char_mode['syst']['name']] = self.ZodiacalLight.calcfZmin(sInds, self.Observatory, TL, self.TimeKeeping, char_mode, modeHashName, koMap[char_mode['syst']['name']], self.koTimes) # find fZmin to use in intTimeFilter
+        self.occ_valfZmin, self.occ_absTimefZmin = self.ZodiacalLight.extractfZmin_fZQuads(self.fZQuads[char_mode['syst']['name']])
         fEZ = self.ZodiacalLight.fEZ0 # grabbing fEZ0
         dMag = self.dMagint[sInds] # grabbing dMag
         WA = self.WAint[sInds] # grabbing WA
-        self.occ_intTimesIntTimeFilter = self.OpticalSystem.calc_intTime(TL, sInds, self.occ_valfZmin, fEZ, dMag, WA, self.mode)*char_mode['timeMultiplier'] # intTimes to filter by
+        self.occ_intTimesIntTimeFilter = self.OpticalSystem.calc_intTime(TL, sInds, self.occ_valfZmin, fEZ, dMag, WA, char_mode)*char_mode['timeMultiplier'] # intTimes to filter by
         self.occ_intTimeFilterInds = np.where((self.occ_intTimesIntTimeFilter > 0)*(self.occ_intTimesIntTimeFilter <= self.OpticalSystem.intCutoff) > 0)[0] # These indices are acceptable for use simulating
 
         # Promote all stars assuming they have known earths

EXOSIMS/ZodiacalLight/Stark.py
@@ -95,17 +95,17 @@ class Stark(ZodiacalLight):
         #Interpolates 2D
         fbeta = griddata(self.points, self.values, list(zip(lon, lat)))
         
-        lam = mode['lam'] # extract wavelength
+        lam = mode['lam']   # extract wavelength
+        BW = mode['BW']     # extract bandwidth
 
         f = 10.**(self.logf(np.log10(lam.to('um').value)))*u.W/u.m**2/u.sr/u.um
         h = const.h                             # Planck constant
         c = const.c                             # speed of light in vacuum
         ephoton = h*c/lam/u.ph                  # energy of a photon
-        F0 = TL.starF0(sInds, mode)             # zero-magnitude star (in ph/s/m2/nm)
+        F0 = TL.F0(BW,lam)                      # zero-magnitude star (sun) (in ph/s/m2/nm)
         f_corr = f/ephoton/F0                   # color correction factor
-        
         fZ = fbeta*f_corr.to('1/arcsec2')
-        
+
         return fZ
 
     def calcfbetaInput(self):
@@ -120,9 +120,10 @@ class Stark(ZodiacalLight):
         lat_pts = np.array([0., 5, 10, 15, 20, 25, 30, 45, 60, 75, 90]) # deg
         y_pts, x_pts = np.meshgrid(lat_pts, lon_pts)
         points = np.array(list(zip(np.concatenate(x_pts), np.concatenate(y_pts))))
-        # create data values, normalized by (90,0) value
+        # create data values, normalized by (90,0) value due to table encoding
         z = Izod/Izod[12,0]
         values = z.reshape(z.size)
+
         return  points, values
 
     def calclogf(self):
@@ -183,12 +184,14 @@ class Stark(ZodiacalLight):
 
         #IF the fZmax File Does Not Exist, Calculate It
         else:
-            self.vprint("Calculating fZmax")
-            if not hasattr(self,'fZ_startSaved'):
-                self.fZ_startSaved = self.generate_fZ(Obs, TL, TK, mode, hashname)
+#            self.vprint("Calculating fZmax")
+#            if not hasattr(self,'fZMap'):
+#                self.fZMap[mode['syst']['name']] = self.generate_fZ(Obs, TL, TK, mode, hashname)
 
-            tmpfZ = np.asarray(self.fZ_startSaved)
-            fZ_matrix = tmpfZ[sInds,:]#Apply previous filters to fZ_startSaved[sInds, 1000]
+            assert np.any(self.fZMap[mode['syst']['name']]) == True, "fZMap does not exist for the mode of interest"
+            
+            tmpfZ = np.asarray(self.fZMap[mode['syst']['name']])
+            fZ_matrix = tmpfZ[sInds,:]#Apply previous filters to fZMap[sInds, 1000]
             
             #Generate Time array heritage from generate_fZ
             startTime = np.zeros(sInds.shape[0])*u.d + TK.currentTimeAbs#Array of current times
@@ -261,13 +264,13 @@ class Stark(ZodiacalLight):
                         fZQuads[i][j][1] = fZQuads[i][j][1]/u.arcsec**2.
             return [fZQuads[i] for i in sInds]
         else:
-#            if not hasattr(self,'fZ_startSaved'):   # if it doesn't have the attribute or if it doesn't have the attribute for the particular mode (change fZ_startSaved to a dictionary that takes in the mode)
-#                self.fZ_startSaved = self.generate_fZ(Obs, TL, TK, mode, hashname)
+#            if not hasattr(self,'fZMap'):   # if it doesn't have the attribute or if it doesn't have the attribute for the particular mode (change fZMap to a dictionary that takes in the mode)
+#                self.fZMap = self.generate_fZ(Obs, TL, TK, mode, hashname)
 
-            assert np.any(self.fZ_startSaved[mode['syst']['name']]) == True, "fZ_startSaved does not exist for the mode of interest"
+            assert np.any(self.fZMap[mode['syst']['name']]) == True, "fZMap does not exist for the mode of interest"
 
-            tmpfZ = np.asarray(self.fZ_startSaved[mode['syst']['name']])
-            fZ_matrix = tmpfZ[sInds,:]#Apply previous filters to fZ_startSaved[sInds, 1000]
+            tmpfZ = np.asarray(self.fZMap[mode['syst']['name']])
+            fZ_matrix = tmpfZ[sInds,:]#Apply previous filters to fZMap[sInds, 1000]
             
             #Generate Time array heritage from generate_fZ
             startTime = np.zeros(sInds.shape[0])*u.d + TK.currentTimeAbs#Array of current times
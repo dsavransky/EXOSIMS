8e98bf117c364d3965256e467e127c67644bcdb1
Dmitry Savransky <dsavransky@gmail.com>
2022-11-30 10:47:18 -0500

    adding Box1D wrapper class

--- EXOSIMS/util/photometricModels.py
+++ EXOSIMS/util/photometricModels.py
@@ -2,11 +2,15 @@
 Various useful photometric models from the literature
 """
 import astropy.units as u
+from synphot.models import Box1D as Box1D_orig
+from synphot.compat import ASTROPY_LT_5_0
+from functools import partial
+import numpy as np
 
 
-def TraubStellarFlux(V, BV, lam):
+def TraubStellarFluxDensity(V, BV, lam):
     """
-    Stellar photon flux at a given wavelength.
+    Stellar photon flux density at a given wavelength.
 
     This implements the model from [Traub2016]_ which has a stated valid range between
     0.4 and 1 um.
@@ -26,8 +30,8 @@ def TraubStellarFlux(V, BV, lam):
 
     Returns:
         ~astropy.units.Quantity:
-            Stellar flux(es) at wavelength lam. This will have the same dimensionality
-            as the V and BV inputs. Default units are ph/s/cm^2/nm
+            Stellar flux desnsity at wavelength lam. This will have the same
+            dimensionality as the V and BV inputs. Default units are ph/s/cm^2/nm
     """
 
     a = 4.01 - (lam.to(u.um).value - 0.55) / 0.770
@@ -36,3 +40,43 @@ def TraubStellarFlux(V, BV, lam):
     f = 10 ** (a - 0.4 * m) * u.ph / u.s / u.cm**2 / u.nm
 
     return f
+
+
+class Box1D(Box1D_orig):
+    """Same as `synphot.models.Box1D`, except with ``step`` input.
+    """
+
+    def __init__(self, step=0.01, *args, **kwargs):
+
+        super(Box1D, self).__init__(*args, **kwargs)
+        self.step = float(step)
+
+    def sampleset(self, step=None, minimal=False):
+        """Return ``x`` array that samples the feature.
+
+        Parameters
+        ----------
+        step : float
+            Distance of first and last points w.r.t. bounding box.
+
+        minimal : bool
+            Only return the minimal points needed to define the box;
+            i.e., box edges and a point outside on each side.
+
+        """
+        if step is None:
+            step = self.step
+
+        if ASTROPY_LT_5_0:
+            w1, w2 = self.bounding_box
+        else:
+            w1, w2 = tuple(self.bounding_box.bounding_box())
+
+        if self._n_models == 1:
+            w = self._calc_sampleset(w1, w2, step, minimal)
+        else:
+            w = list(
+                map(partial(self._calc_sampleset, step=step, minimal=minimal), w1, w2)
+            )
+
+        return np.asarray(w)

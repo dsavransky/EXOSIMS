b603bae77f3d35bfff548c190cb96d9a6b5c742b
Dmitry Savransky <dsavransky@gmail.com>
2022-12-22 10:59:14 -0500

    adding various useful photometry methods

--- EXOSIMS/util/radialfun.py
+++ EXOSIMS/util/radialfun.py
@@ -3,6 +3,28 @@ Utilities for radial computations on rectangular data arrays
 """
 
 import numpy as np
+from scipy.interpolate import RectBivariateSpline
+
+
+def pixel_dists(dims, center):
+    """
+    Compute pixel distances from center of an image
+
+    Args:
+        dims (tuple(float,float)):
+            Image dimensions
+        center (list(float,float)):
+            [x,y] pixel coordinates of center
+
+    Returns:
+        numpy.ndarray:
+            Array of dimension dims with distance from center of each pixel
+    """
+
+    y, x = np.indices(dims)
+    r = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2)
+
+    return r
 
 
 def radial_average(im, center=None, nbins=None):
@@ -41,8 +63,7 @@ def radial_average(im, center=None, nbins=None):
         nbins = int(np.floor(np.max(dims) / 2))
 
     # compute pixel distances from center
-    y, x = np.indices(dims)
-    r = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2)
+    r = pixel_dists(dims, center)
 
     # define bins and digitize distanes
     bins = np.linspace(0, np.max(r), nbins + 1)
@@ -58,3 +79,117 @@ def radial_average(im, center=None, nbins=None):
         means[j - 1] = np.nanmean(imflat[inds == j])
 
     return means, bins, bincents
+
+
+def circ_aperture(im, rho, center, return_sum=False):
+    """
+    Extract pixels in circular aperture
+
+    Args:
+        im (numpy.ndarray):
+            The input image. Must be 2-dimensional
+        rho (float):
+            Radius of aperture (in pixels)
+        center (list(float,float):
+            [x,y] pixel coordinates of center of aperture
+        return_sum (bool):
+            Return sum. Defaults False: returns all pixels in aperture.
+
+    Returns:
+        numpy.ndarray:
+            1-dimensional array of pixel values inside aperture
+    """
+
+    # compute pixel distances from center
+    r = pixel_dists(im.shape, center)
+
+    pix = im[r <= rho]
+
+    if return_sum:
+        out = np.sum(pix[np.isfinite(pix)])
+    else:
+        out = pix
+
+    return out
+
+
+def com(im0, fill_val=0):
+    """
+    Find the center-of-mass centroid of an image
+
+    Args:
+        im (numpy.ndarray):
+            The input image. Must be 2-dimensional
+        fill_val (float):
+            Replace any non finite values in the image with this value before
+            resampling. Defaults to zero.
+
+    Returns:
+        list:
+            [x,y] pixel coordinates of COM
+    """
+
+    # operate on input copy and zero out bad values
+    im = im0.copy()
+    im[~np.isfinite(im)] = fill_val
+
+    y, x = np.indices(im.shape)
+
+    return [np.sum(im * inds) / np.sum(im) for inds in [x, y]]
+
+
+def genwindow(dims):
+    """
+    Create a 2D Hann window of the given dimensions
+
+    Args:
+        dims (tuple or list):
+            2-element dimensions of window (can be the .shape output of an ndarray)
+
+    Returns:
+        numpy.ndarray:
+            Window of dimensions ``dims``.
+
+    """
+
+    window = np.ones(dims)
+    y, x = np.indices(dims)
+    for dim, inds in zip(dims, [x, y]):
+        window *= 0.5 - 0.5 * np.cos(2 * np.pi * inds / dim)
+    window = np.sqrt(window)
+
+    return window
+
+
+def resample_image(im, resamp=2, fill_val=0):
+    """
+    Create a resampled image
+
+    Args:
+        im (numpy.ndarray):
+            The input image. Must be 2-dimensional
+        resamp (float):
+            Resampling factor. Must be >=1. Defaults to 2
+        fill_val (float):
+            Replace any non finite values in the image with this value before
+            resampling. Defaults to zero.
+
+    Returns:
+        numpy.ndarray:
+            Resampled image.
+
+    """
+
+    assert resamp >= 1, "resamp must be >= 1"
+    if resamp == 1:
+        return im
+
+    dims = im.shape
+    newdims = [(d - 1) * resamp + 1 for d in dims]
+
+    imc = im.copy()
+    imc[~np.isfinite(imc)] = fill_val
+    sp = RectBivariateSpline(np.arange(dims[0]), np.arange(dims[0]), imc)
+    imresamp = sp(np.arange(newdims[0]) / resamp, np.arange(newdims[1]) / resamp)
+
+    return imresamp

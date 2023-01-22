ec4fef3e35922a239f70df8e63b6fb4bf32685d2
Sachin Kelkar <ssk242@cornell.edu>
2023-01-21 20:10:18 -0500

    updated methods

--- EXOSIMS/Observatory/mutliSSObs.py
+++ EXOSIMS/Observatory/mutliSSObs.py
@@ -11,6 +11,8 @@ class multiSS_observatory(SotoStarshade):
         dryMass=[3400.0, 3400.0],
         counter=0,
         counter_1=0,
+        counter_2=0,
+        counter_3=0,
         **specs
     ):
         SotoStarshade.__init__(self, **specs)
@@ -19,6 +21,8 @@ class multiSS_observatory(SotoStarshade):
         self.scMass = scMass * u.kg
         self.counter = counter
         self.counter_1 = counter_1
+        self.counter_2 = counter_2
+        self.counter_3 = counter_3
         # occulters' dry mass(kg)
         self.dryMass = np.array(dryMass) * u.kg
 
@@ -143,6 +147,82 @@ class multiSS_observatory(SotoStarshade):
             dF_axial = np.abs(dF_axial)
             self.counter = 0
             return dF_lateral, dF_axial
+        
+    def star_angularSep(self, TL, old_sInd, sInds, currentTime):
+        """Finds angular separation from old star to given list of stars
+
+        This method returns the angular separation from the last observed
+        star to all others on the given list at the currentTime.
+
+        Args:
+            TL (:ref:`TargetList`):
+                TargetList class object
+            old_sInd (int):
+                Integer index of the last star of interest
+            sInds (~numpy.ndarray(int)):
+                Integer indices of the stars of interest
+            currentTime (~astropy.time.Time):
+                Current absolute mission time in MJD
+
+        Returns:
+            float:
+                Angular separation between two target stars
+        """
+        if self.counter_3 == 0:
+
+            if old_sInd is None:
+                sd = np.zeros(len(sInds)) * u.rad
+            else:
+                # position vector of previous target star
+                r_old = TL.starprop(self.DRM[-1]['star_sInd'], currentTime)[0]
+                u_old = r_old.to("AU").value / np.linalg.norm(r_old.to("AU").value)
+                # position vector of new target stars
+                r_new = TL.starprop(sInds, currentTime)
+                u_new = (
+                    r_new.to("AU").value.T / np.linalg.norm(r_new.to("AU").value, axis=1)
+                ).T
+                # angle between old and new stars
+                sd = np.arccos(np.clip(np.dot(u_old, u_new.T), -1, 1)) * u.rad
+
+                # A-frame
+                a1 = u_old / np.linalg.norm(u_old)  # normalized old look vector
+                a2 = np.array([a1[1], -a1[0], 0])  # normal to a1
+                a3 = np.cross(a1, a2)  # last part of the A basis vectors
+
+                # finding sign of angle
+                # The star angular separation can be negative
+                u2_Az = np.dot(a3, u_new.T)
+                sgn = np.sign(u2_Az)
+                sgn[np.where(sgn == 0)] = 1
+                sd = sgn * sd
+            self.counter_3 = self.counter_3+1
+            return sd
+        
+        else :
+            # position vector of previous target star
+            r_old = TL.starprop(self.DRM[-2]['star_sInd'], currentTime)[0]
+            u_old = r_old.to("AU").value / np.linalg.norm(r_old.to("AU").value)
+            # position vector of new target stars
+            r_new = TL.starprop(sInds, currentTime)
+            u_new = (
+                r_new.to("AU").value.T / np.linalg.norm(r_new.to("AU").value, axis=1)
+            ).T
+            # angle between old and new stars
+            sd = np.arccos(np.clip(np.dot(u_old, u_new.T), -1, 1)) * u.rad
+
+            # A-frame
+            a1 = u_old / np.linalg.norm(u_old)  # normalized old look vector
+            a2 = np.array([a1[1], -a1[0], 0])  # normal to a1
+            a3 = np.cross(a1, a2)  # last part of the A basis vectors
+
+            # finding sign of angle
+            # The star angular separation can be negative
+            u2_Az = np.dot(a3, u_new.T)
+            sgn = np.sign(u2_Az)
+            sgn[np.where(sgn == 0)] = 1
+            sd = sgn * sd
+        self.counter_3 = 0
+        return sd
 
     def mass_dec(self, dF_lateral, t_int):
         """Returns mass_used and deltaV
@@ -180,3 +260,184 @@ class multiSS_observatory(SotoStarshade):
             deltaV = (dF_lateral / self.scMass[1] * t_int).to("km/s")
             self.counter_1 = 0
             return intMdot, mass_used, deltaV
+        
+    def calculate_slewTimes(self, TL, old_sInd, sInds, sd, obsTimes, currentTime):
+        """Finds slew times and separation angles between target stars
+
+        This method determines the slew times of an occulter spacecraft needed
+        to transfer from one star's line of sight to all others in a given
+        target list.
+
+        Args:
+            TL (:ref:`TargetList`):
+                TargetList class object
+            old_sInd (int):
+                Integer index of the most recently observed star
+            sInds (~numpy.ndarray(int)):
+                Integer indices of the star of interest
+            sd (~astropy.units.Quantity):
+                Angular separation between stars in rad
+            obsTimes (~astropy.time.Time(~numpy.ndarray)):
+                Observation times for targets.
+            currentTime (astropy Time):
+                Current absolute mission time in MJD
+
+        Returns:
+            ~astropy.units.Quantity:
+                Time to transfer to new star line of sight in units of days
+        """
+        if self.counter_2 == 0 :
+
+            self.ao[0] = self.thrust / self.scMass[0]
+            slewTime_fac = (
+                (
+                    2.0
+                    * self.occulterSep
+                    / np.abs(self.ao)
+                    / (self.defburnPortion / 2.0 - self.defburnPortion**2.0 / 4.0)
+                )
+                .decompose()
+                .to("d2")
+            )
+
+            if old_sInd is None:
+                slewTimes = np.zeros(TL.nStars) * u.d
+            else:
+                # calculate slew time
+                slewTimes = np.sqrt(
+                    slewTime_fac * np.sin(abs(sd) / 2.0)
+                )  # an issue exists if sd is negative
+
+                # The following are debugging
+                assert (
+                    np.where(np.isnan(slewTimes))[0].shape[0] == 0
+                ), "At least one slewTime is nan"
+            self.counter_2 = self.counter_2+1
+            return slewTimes
+        
+        else :
+            self.ao[1] = self.thrust / self.scMass[1]
+            slewTime_fac = (
+                (
+                    2.0
+                    * self.occulterSep
+                    / np.abs(self.ao)
+                    / (self.defburnPortion / 2.0 - self.defburnPortion**2.0 / 4.0)
+                )
+                .decompose()
+                .to("d2")
+            )
+
+            if old_sInd is None:
+                slewTimes = np.zeros(TL.nStars) * u.d
+            else:
+                # calculate slew time
+                slewTimes = np.sqrt(
+                    slewTime_fac * np.sin(abs(sd) / 2.0)
+                )  # an issue exists if sd is negative
+
+                # The following are debugging
+                assert (
+                    np.where(np.isnan(slewTimes))[0].shape[0] == 0
+                ), "At least one slewTime is nan"
+            self.counter_2 = 0
+            return slewTimes
+            
+    
+    def log_occulterResults(self, DRM, slewTimes, sInd, sd, dV):
+        """Updates the given DRM to include occulter values and results
+
+        Args:
+            DRM (dict):
+                Design Reference Mission, contains the results of one complete
+                observation (detection and characterization)
+            slewTimes (astropy.units.Quantity):
+                Time to transfer to new star line of sight in units of days
+            sInd (int):
+                Integer index of the star of interest
+            sd (astropy.units.Quantity):
+                Angular separation between stars in rad
+            dV (astropy.units.Quantity):
+                Delta-V used to transfer to new star line of sight in units of m/s
+
+        Returns:
+            dict:
+                Design Reference Mission dictionary, contains the results of one
+                complete observation (detection and characterization)
+
+        """
+
+        DRM["slew_time"] = slewTimes.to("day")
+        DRM["slew_angle"] = sd.to("deg")
+
+        slew_mass_used = slewTimes * self.defburnPortion * self.flowRate
+        DRM["slew_dV"] = (slewTimes * self.ao * self.defburnPortion).to("m/s")
+        DRM["slew_mass_used"] = slew_mass_used.to("kg")
+        self.scMass = self.scMass - slew_mass_used
+        DRM["scMass"] = self.scMass.to("kg")
+        if self.twotanks:
+            self.slewMass = self.slewMass - slew_mass_used
+            DRM["slewMass"] = self.slewMass.to("kg")
+        return DRM
+
+    def refuel_tank(self, TK, tank=None):
+        """Attempt to refuel a fuel tank and report status
+
+        Args:
+            TK (:ref:`TimeKeeping`):
+                TimeKeeping object. Not used in prototype but an input for any
+                implementations that wish to do time-aware operations.
+            tank (str, optional):
+                Either 'sk' or 'slew' when ``twotanks`` is True. Otherwise, None.
+                Defaults None
+
+        Returns:
+            bool:
+                True represents successful refeuling. False means refueling is not
+                possible for selected tank.
+        """
+
+        if not (self.allowRefueling):
+            return False
+
+        if self.external_fuel_mass <= 0 * u.kg:
+            return False
+
+        if tank is not None:
+            assert tank.lower() in ["sk", "slew"], "Tank must be 'sk' or 'slew'."
+            assert self.twotanks, "You may only specify a tank when twotanks is True."
+
+            if tank == "sk":
+                tank_mass = self.skMass
+                tank_capacity = self.skMaxFuelMass
+                tank_name = "stationkeeping"
+            else:
+                tank_mass = self.slewMass
+                tank_capacity = self.slewMaxFuelMass
+                tank_name = "slew"
+        else:
+            tank_mass = self.scMass - self.dryMass
+            tank_capacity = self.maxFuelMass
+            tank_name = ""
+
+        # Add as much fuel as can fit in the tank (plus any currently carried negative
+        # value, or whatever remains in the external tank)
+        topoff = (
+            np.min(
+                [
+                    self.external_fuel_mass.to(u.kg).value,
+                    (tank_capacity - tank_mass).to(u.kg).value,
+                ]
+            )
+            * u.kg
+        )
+        assert topoff >= 0 * u.kg, "Topoff calculation produced negative result."
+
+        self.external_fuel_mass -= topoff
+        tank_mass += topoff
+        if tank is not None:
+            self.scMass += topoff
+        self.vprint("{} {} fuel added".format(topoff, tank_name))
+        self.vprint("{} remaining in external tank.".format(self.external_fuel_mass))
+
+        return True

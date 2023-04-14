b21ce817a25767b37db7e5f5ce92ff6f3937d84f
Dmitry Savransky <dsavransky@gmail.com>
2023-04-14 08:55:33 -0700

    adding utility attributes default_mode and int_tmin to TargetList.  Updating  process_opticalsys_package to handle the case where the intensity diameter is stored as a row rather than column array. bumping version for bugfix release.

--- EXOSIMS/__init__.py
+++ EXOSIMS/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 name = "EXOSIMS"
-__version__ = "3.1.0"
+__version__ = "3.1.1"
 
 # Set up a default logging handler to avoid "No handler found" warnings.
 # Other handlers can add to this one.

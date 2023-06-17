2d673e29e429c69e650436822c22b14a31eb395a
Corey Spohn <cas584@cornell.edu>
2023-06-17 15:27:48 -0400

    Allowing vprint to be pickled

--- EXOSIMS/util/vprint.py
+++ EXOSIMS/util/vprint.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-def vprint(verbose):
+class vprint:
     """This function is equivalent to the python print function, with an extra boolean
     parameter that toggles the print when it is required.
 
@@ -15,14 +15,21 @@ def vprint(verbose):
             The new print function with one argument, the string to be printed (toprint)
 
     """
-    if verbose is True:
 
-        def f(toprint):
+    def __init__(self, verbose):
+        self.verbose = verbose
+        # if verbose:
+        #     self.verbose = True
+        # else:
+
+    def __call__(self, toprint):
+        if self.verbose:
             print(toprint)
 
-    else:
-
-        def f(toprint):
-            pass
-
-    return f
+    # if verbose is True:
+    #     def f(toprint):
+    #         print(toprint)
+    # else:
+    #     def f(toprint):
+    #         pass
+    # return f

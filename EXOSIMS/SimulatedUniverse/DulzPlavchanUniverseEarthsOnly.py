28873e92553d9375ec78612c74e312f5547a271a
Corey Spohn <cas584@cornell.edu>
2023-06-21 18:46:55 -0400

    Adding keyword and methods to force systems to have Earth-like planets

--- EXOSIMS/SimulatedUniverse/DulzPlavchanUniverseEarthsOnly.py
+++ EXOSIMS/SimulatedUniverse/DulzPlavchanUniverseEarthsOnly.py
@@ -11,9 +11,10 @@ class DulzPlavchanUniverseEarthsOnly(SimulatedUniverse):
             indicates whether to use Earth's phase function or not
     """
 
-    def __init__(self, earthPF=True, **specs):
+    def __init__(self, earthPF=True, guarantee_earths=False, **specs):
 
         self.earthPF = earthPF
+        self.guarantee_earths = guarantee_earths
         SimulatedUniverse.__init__(self, **specs)
 
     def gen_physical_properties(self, **specs):
@@ -28,23 +29,29 @@ class DulzPlavchanUniverseEarthsOnly(SimulatedUniverse):
         for j, n in enumerate(targetSystems):
             plan2star = np.hstack((plan2star, [j] * n))
 
-        a, e, p, Rp = PPop.gen_plan_params(len(plan2star))
+        if self.guarantee_earths:
+            a, e, p, Rp, plan2star = self.gen_earths(len(targetSystems), plan2star)
+            self.a = a * u.AU
+            self.e = np.array(e)
+            self.p = np.array(p)
+            self.Rp = Rp * u.R_earth
+        else:
+            a, e, p, Rp = PPop.gen_plan_params(len(plan2star))
 
-        # filter to just Earth candidates
-        # modify as needed NB: a has no yet been scaled by \sqrt{L}
-        # so effectively everything is at 1 solar luminosity
-        inds = (
-            (a < 1.67 * u.AU)
-            & (a > 0.95 * u.AU)
-            & (Rp < 1.4 * u.R_earth)
-            & (Rp > 0.8 / np.sqrt(a.value) * u.R_earth)
-        )
-
-        self.a = a[inds]
-        self.e = e[inds]
-        self.p = p[inds]
-        self.Rp = Rp[inds]
-        plan2star = plan2star[inds]
+            # filter to just Earth candidates
+            # modify as needed NB: a has no yet been scaled by \sqrt{L}
+            # so effectively everything is at 1 solar luminosity
+            inds = (
+                (a < 1.67 * u.AU)
+                & (a > 0.95 * u.AU)
+                & (Rp < 1.4 * u.R_earth)
+                & (Rp > 0.8 / np.sqrt(a.value) * u.R_earth)
+            )
+            self.a = a[inds]
+            self.e = e[inds]
+            self.p = p[inds]
+            self.Rp = Rp[inds]
+            plan2star = plan2star[inds]
 
         self.plan2star = plan2star.astype(int)
         self.sInds = np.unique(self.plan2star)
@@ -72,3 +79,41 @@ class DulzPlavchanUniverseEarthsOnly(SimulatedUniverse):
             )  # Used to switch select specific phase function for each planet
         else:
             self.phiIndex = np.asarray([])
+
+    def gen_earths(self, nsystems, plan2star):
+        # Hacky way to guarantee that an Earth-like planet is in every system
+        # a, e, p, Rp = (
+        #     np.zeros(nsystems),
+        #     np.zeros(nsystems),
+        #     np.zeros(nsystems),
+        #     np.zeros(nsystems),
+        # )
+        a, e, p, Rp, new_plan2star = [], [], [], [], []
+        for ind in range(nsystems):
+            _a, _e, _p, _Rp = self.create_earths(sum(plan2star == ind))
+            a.extend(_a)
+            e.extend(_e)
+            p.extend(_p)
+            Rp.extend(_Rp)
+            new_plan2star.extend(np.ones(len(_a), dtype=int) * ind)
+        return a, e, p, Rp, np.array(new_plan2star, dtype=int)
+
+    def create_earths(self, nplans):
+        _a, _e, _p, _Rp = self.PlanetPopulation.gen_plan_params(nplans)
+
+        success = (
+            (_a < 1.67 * u.AU)
+            & (_a > 0.95 * u.AU)
+            & (_Rp < 1.4 * u.R_earth)
+            & (_Rp > 0.8 / np.sqrt(_a.value) * u.R_earth)
+        )
+        if np.any(success):
+            success_inds = np.where(success)[0]
+            return (
+                _a[success_inds].to(u.AU).value,
+                _e[success_inds],
+                _p[success_inds],
+                _Rp[success_inds].to(u.R_earth).value,
+            )
+        else:
+            return self.create_earths(nplans)

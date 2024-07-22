cc43bdb080714706953d546303ecb43c5d1847fe
ahnikagee <158200287+ahnikagee@users.noreply.github.com>
2024-07-22 10:14:00 -0400

    Update TargetList.py

--- EXOSIMS/Prototypes/TargetList.py
+++ EXOSIMS/Prototypes/TargetList.py
@@ -1617,9 +1617,9 @@ class TargetList(object):
         elif self.massLuminosityRelationship == "Fernandes2021":
             # only good for FGK
             # 'approximate' stellar mass without error
-            self.MsEst = (10 ** ((0.219 * np.log10(self.L))
-                                    + (0.063 * ((np.log10(self.L)) ** 2))
-                                    - (0.119 * ((np.log10(self.L)) ** 3)))) * u.solMass
+            self.MsEst = (10 ** ((0.219 * np.log10(self.L)) +
+                                 (0.063 * ((np.log10(self.L)) ** 2)) -
+                                 (0.119 * ((np.log10(self.L)) ** 3)))) * u.solMass
             # error distribution in literature as 3% in approxoimate masses
             err = (np.random.random(len(self.L)) * 2.0 - 1.0) * 0.03
             self.MsTrue = (1.0 + err) * self.MsEst
